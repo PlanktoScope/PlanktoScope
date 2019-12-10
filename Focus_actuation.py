be2e0844e3523f97e5f534a02ebe16484f2eb11d
tpollina <thibaut.pollina@gmail.com>
2019-12-10 15:20:42 -0800

    Update Focus_actuation.py

--- Focus_actuation.py
+++ Focus_actuation.py
@@ -1,27 +1,30 @@
 from adafruit_motor import stepper
 from adafruit_motorkit import MotorKit
 from time import sleep
+
 kit = MotorKit()
 
 stage = kit.stepper1
 
 stage.release()
 
-def focus_actuation(steps,orientation):  
+def focus(steps,orientation):
+    #0.25mm/step
+    #31um/microsteps
 
     stage.release()
     
-    if orientation == 'FORWARD':
+    if orientation == 'up':
         for i in range(steps):
             stage.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
             sleep(0.001)
             
-    if orientation == 'BACKWARD':
+    if orientation == 'down':
         for i in range(steps):
             stage.onestep(direction=stepper.BACKWARD, style=stepper.MICROSTEP)
             sleep(0.001)
             
     stage.release()
     
-focus_actuation(1000,'FORWARD')
-focus_actuation(1000,'BACKWARD')
+focus_actuation(1000,'up')
+focus_actuation(1000,'down')
