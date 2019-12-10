8e9bd56584a8998d4cb56f5d9f7b6f7d54e529f8
tpollina <thibaut.pollina@gmail.com>
2019-12-10 15:14:36 -0800

    Update pump_volume_flowrate.py

--- pump_volume_flowrate.py
+++ pump_volume_flowrate.py
@@ -1,43 +1,34 @@
 from adafruit_motor import stepper
 from adafruit_motorkit import MotorKit
 from time import sleep
-from datetime import datetime
-import time
 
 kit = MotorKit()
 
-pump = kit.stepper2
+pump_stepper = kit.stepper2
 
-pump.release()
+pump_stepper.release()
 
-
-
-def pump_volume(vol, flowrate):
+def pump(vol, flowrate, dir):
+    
+    if dir == "foward":
+        dir=stepper.FORWARD
+    if dir == "backward":
+        dir=stepper.BACKWARD
+        
     nb_step=vol*507 #if sleep(0.05) in between 2 steps
-    #35000steps for 69g 
+    #35000steps for 69g
+    
     #nb_step=vol*460 if sleep(0) in between 2 steps
     duration=(vol*60)/flowrate
-    delay=(duration/nb_step)-0.005
-    #flowrate is about 2.2ml/min
-    for i in range(nb_step):
-        pump.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
-        sleep(delay)
-    print(delay)
-    sleep(1)
-    pump.release()
     
-volume=1
-
-flowrate=1 #20 is the max
-
-start = time.time()
-
-pump_volume(volume, flowrate)
-
-end = time.time()
-
-duration = end-start
-
-print(duration)
-
-
+    delay=(duration/nb_step)-0.005
+    
+    for i in range(nb_step):
+        pump_stepper.onestep(direction=dir, style=stepper.DOUBLE)
+        sleep(delay)
+        
+    sleep(1)
+    pump_stepper.release()
+    
+#volume, flowrate (from 0 to 20), direction (foward or backward)
+pump(1, 10, 'foward')
