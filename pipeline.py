dce6a51af9dafd2e2599e3618c6dfa56007c37da
tpollina <thibaut.pollina@gmail.com>
2019-12-10 15:24:23 -0800

    Create pipeline.py

---
+++ pipeline.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import sys
+
+#get the value of the first parameter which is for example an integer
+value = int(sys.argv[1])
+
+#get the value of the first parameter which is for example a string 
+text = str(sys.argv[2])
+
+#Execute a python cmd with the previous defined variables from php
