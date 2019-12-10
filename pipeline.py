37bc5546c96dff8f09c199aa3e9880481b399368
tpollina <thibaut.pollina@gmail.com>
2019-12-10 15:29:34 -0800

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -6,6 +6,6 @@ import sys
 value = int(sys.argv[1])
 
 #get the value of the first parameter which is for example a string 
-text = str(sys.argv[2])
+string = str(sys.argv[2])
 
 #Execute a python cmd with the previous defined variables from php
