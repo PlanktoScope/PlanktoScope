39fef41e38572204b029709d320cf39e0cc2c07b
tpollina <thibaut.pollina@gmail.com>
2019-12-10 17:22:06 -0800

    Update pipeline.js

--- pipeline/pipeline.js
+++ pipeline/pipeline.js
@@ -1,10 +1,21 @@
+$(document).ready(function(){
+
+	$( "#send_nb_step" ).click(function() {
+		pipeline()
+	});
+	
+	
+
 function pipeline() {
-		var value = $("#value").val();
-		var string = $("#string").text();
+		var value = $("#nb_step").val();
+		var orientation = $("#orientation").val();
     
-		console.log("pipeline.php?value="+value+"&string="+string);
+		console.log("pipeline.php?nb_step="+value+"&orientation="+orientation);
 		
         var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", "pipeline.php?value="+value+"&string="+string, true);
+        xmlhttp.open("GET", "pipeline.php?nb_step="+value+"&orientation="+orientation, true);
         xmlhttp.send();
 	}
+
+
+});
