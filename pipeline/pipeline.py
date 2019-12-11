9c4e387f4babf5b4de0b7d1da1b87cb0487b9f29
tpollina <thibaut.pollina@gmail.com>
2019-12-10 17:22:29 -0800

    Update pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python
-
+from adafruit_motor import stepper
+from adafruit_motorkit import MotorKit
+from time import sleep
 import sys
 
-#get the value of the first parameter which is for example an integer
-value = int(sys.argv[1])
+nb_step = int(sys.argv[1])
+orientation = str(sys.argv[2])
 
-#get the value of the first parameter which is for example a string 
-string = str(sys.argv[2])
 
 #Execute a python cmd with the previous defined variables from php
+
+
+kit = MotorKit()
+
+stage = kit.stepper1
+
+stage.release()
+
+def focus(steps,orientation):
+    #0.25mm/step
+    #31um/microsteps
+
+    stage.release()
+    
+    if orientation == 'up':
+        for i in range(steps):
+            stage.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
+            sleep(0.001)
+            
+    if orientation == 'down':
+        for i in range(steps):
+            stage.onestep(direction=stepper.BACKWARD, style=stepper.MICROSTEP)
+            sleep(0.001)
+            
+    stage.release()
+    
+focus(nb_step,orientation)
