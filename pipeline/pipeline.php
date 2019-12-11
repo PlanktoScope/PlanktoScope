673b83c550561d5109895129d1572909907c9b58
tpollina <thibaut.pollina@gmail.com>
2019-12-10 17:23:15 -0800

    Update pipeline.php

--- pipeline/pipeline.php
+++ pipeline/pipeline.php
@@ -1,12 +1,7 @@
 <?php
-// get the q parameter from GET
+$nb_step = $_REQUEST["nb_step"];
+$orientation = $_REQUEST["orientation"];
 
-// Get the first parameter which is an integer
-$value = $_REQUEST["value"];
 
-// Get the second parameter which is a string
-$string = $_REQUEST["string"];
-
-//Run the command line for the python script with the value as parameter
-$output = shell_exec('/usr/bin/python3 /var/www/html/I2C/pipeline.py '.$value.' '.$string);
+$output = shell_exec('/usr/bin/python3 pipeline.py '.$nb_step.' '.$orientation);
 ?>
