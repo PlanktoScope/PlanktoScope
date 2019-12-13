bb893b19fe61bc54b1db5969fbc938d8991b7e22
tpollina <thibaut.pollina@gmail.com>
2019-12-13 14:20:31 -0800

    Update kill_image.sh

--- scripts/kill_image.sh
+++ scripts/kill_image.sh
@@ -7,6 +7,6 @@ sample_project=$1
 sample_id=$2
 acq_id=$3
 
-kill -9 `ps ax | grep "python3.7 image.py" | head -1 | awk '{print $1}'`
+kill -9 `ps ax | grep "image.py" | head -1 | awk '{print $1}'`
 
 rm -r $HOME/PlanktonScope/acquisitions/$sample_project/$sample_id/$acq_id
