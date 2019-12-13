7141c117e799ed01a331dc8b6ed2a2571c616279
tpollina <thibaut.pollina@gmail.com>
2019-12-13 10:49:19 -0800

    Create light.py

---
+++ scripts/light.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import sys
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+state = int(sys.argv[1])
+
+
+def light(state):
+    
+    if state == "on":
+        GPIO.output(21,GPIO.HIGH)
+    if state == "off":
+        GPIO.output(21,GPIO.LOW)
+        
+light(state)
