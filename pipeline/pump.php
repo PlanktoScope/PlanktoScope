626092cb1926c4b283af2504ea1f73f657c29056
tpollina <thibaut.pollina@gmail.com>
2020-01-15 15:41:47 +0100

    Delete pump.php

--- pipeline/pump.php
+++
@@ -1,8 +0,0 @@
-<?php
-$volume = $_REQUEST["volume"];
-$flowrate = $_REQUEST["flowrate"];
-$direction = $_REQUEST["direction"];
-
-
-$output = shell_exec('/usr/bin/python3 /var/www/html/pipeline/pump.py '.$volume.' '.$flowrate.' '.$direction);
-?>
