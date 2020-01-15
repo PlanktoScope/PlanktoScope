7cec6670be9138f8f568332e5b9041c25c37dbb8
tpollina <thibaut.pollina@gmail.com>
2020-01-15 15:41:56 +0100

    Delete focus.php

--- pipeline/focus.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nb_step = $_REQUEST["nb_step"];
-$orientation = $_REQUEST["orientation"];
-$toggler = $_REQUEST["toggler"];
-
-
-$output = shell_exec('/usr/bin/python3 /var/www/html/pipeline/focus.py '.$nb_step.' '.$orientation.' '.$toggler);
-?>
