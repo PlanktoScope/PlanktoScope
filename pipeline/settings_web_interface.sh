60bdc3ede60b0aa9f18e2dfaba9100a863b1b6e5
thibaut pollina <thibaut.pollina@gmail.com>
2020-02-09 00:15:36 -0800

    Clean up useless files

--- pipeline/settings_web_interface.sh
+++
@@ -1,9 +0,0 @@
-
-cd ~
-sudo touch starting.sh
-sudo chmod 777 starting.sh
-sudo echo 'sudo chmod 777 /dev/i2c-1' >> starting.sh
-
-sudo echo './starting.sh' >> .bashrc
-
-sudo reboot
--- scripts/killer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash    
-#Run this script using this type of command line : bash killer.sh name_of_the_python_script_to_kill.py
-
-scrip_name=$1
-
-kill -9 `ps ax | grep "$scrip_name" | head -1 | awk '{print $1}'`
-
-python3.7 $HOME/PlanktonScope/scripts/recover.py
