6c68c77da8f43b1fb244ab2f5195a69d333d30d9
Romain Bazile <gromain.baz@gmail.com>
2020-09-17 19:31:41 +0200

    cosmetic update

--- scripts/planktonscope/pump.py
+++ scripts/planktonscope/pump.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-#Strating pumping foward a vol of 24ml with a flowrate of 3.2ml/min, use this command line : 
-#python3.7 path/to/file/pump.py 24 3.2 foward
+# Strating pumping foward a vol of 24ml with a flowrate of 3.2ml/min, use this command line :
+# python3.7 path/to/file/pump.py 24 3.2 foward
 
 from adafruit_motor import stepper
 from adafruit_motorkit import MotorKit
@@ -19,28 +19,29 @@ pump_stepper = kit.stepper1
 
 pump_stepper.release()
 
+
 def pump(volume, flowrate, action):
-    
-    if action == "foward":
-        action=stepper.BACKWARD
+
+    if action == "forward":
+        action = stepper.BACKWARD
     if action == "backward":
-        action=stepper.FORWARD
-        
-    nb_step=volume*507 #if sleep(0.05) in between 2 steps
-    #35000steps for 69g
-    
-    #nb_step=vol*460 if sleep(0) in between 2 steps
-    duration=(volume*60)/flowrate
-    
-    delay=(duration/nb_step)-0.005
-    
+        action = stepper.FORWARD
+
+    nb_step = volume * 507  # if sleep(0.05) in between 2 steps
+    # 35000steps for 69g
+
+    # nb_step=vol*460 if sleep(0) in between 2 steps
+    duration = (volume * 60) / flowrate
+
+    delay = (duration / nb_step) - 0.005
+
     for i in range(nb_step):
         pump_stepper.onestep(direction=action, style=stepper.DOUBLE)
         sleep(delay)
-        
+
     sleep(1)
     pump_stepper.release()
-    
-#volume, flowrate (from 0 to 20), direction (foward or backward)
-pump(volume, flowrate, action)
 
+
+# volume, flowrate (from 0 to 20), direction (foward or backward)
+pump(volume, flowrate, action)
