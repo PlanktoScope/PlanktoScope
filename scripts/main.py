d21cb89636f729bb0567d3b540d6f640c19882cb
Romain Bazile <gromain.baz@gmail.com>
2020-09-21 16:37:49 +0200

    main: management of signal shutdown

--- scripts/main.py
+++ scripts/main.py
@@ -35,28 +35,69 @@ import planktoscope.stepper
 # Import the planktonscope LED module
 import planktoscope.light
 
-# Starts the stepper process for actuators
-logger.info("Starting the stepper control process")
-stepper_thread = planktoscope.stepper.StepperProcess()
-stepper_thread.start()
+# Time module so we can sleep
+import time
 
-# Streaming server creation
-output = planktoscope.streamer.StreamingOutput()
-address = ("", 8000)
-server = planktoscope.streamer.StreamingServer(
-    address, planktoscope.streamer.StreamingHandler
-)
+# signal module is used to managed SINGINT/SIGTERM
+import signal
 
-# Starts the streaming server process
-logger.info("Starting the streaming server process")
-streaming_thread = multiprocessing.Process(target=server.serve_forever)
-streaming_thread.start()
+# global variable that keeps the wheels spinning
+run = True
 
-# Starts the imager control process
-logger.info("Starting the imager control process")
-imager_thread = planktoscope.imager.ImagerProcess()
-imager_thread.start_camera(output)
-imager_thread.start()
 
-while True:
-    pass
+def handler_stop_signals(signum, frame):
+    """This handler simply stop the forever running loop in __main__"""
+    global run
+    run = False
+
+
+if __name__ == "__main__":
+    logger.info("Starting up the PlanktoScope management script")
+    logger.info("Welcome!")
+
+    logger.info("Initialising signals handling (step 1/4)")
+    signal.signal(signal.SIGINT, handler_stop_signals)
+    signal.signal(signal.SIGTERM, handler_stop_signals)
+
+    # Starts the stepper process for actuators
+    logger.info("Starting the stepper control process (step 2/4)")
+    stepper_thread = planktoscope.stepper.StepperProcess()
+    stepper_thread.start()
+
+    # Streaming server creation
+    output = planktoscope.streamer.StreamingOutput()
+    address = ("", 8000)
+    server = planktoscope.streamer.StreamingServer(
+        address, planktoscope.streamer.StreamingHandler
+    )
+
+    # Starts the streaming server process
+    logger.info("Starting the streaming server process (step 3/4)")
+    streaming_thread = multiprocessing.Process(target=server.serve_forever)
+    streaming_thread.start()
+
+    # Starts the imager control process
+    logger.info("Starting the imager control process (step 4/4)")
+    imager_thread = planktoscope.imager.ImagerProcess()
+    imager_thread.start_camera(output)
+    imager_thread.start()
+
+    logger.info("Looks like everything is set up and running, have fun!")
+
+    while run:
+        # TODO implement checks on the different processes started before
+        # If they are not alive, we need to restart them
+        logger.trace("Running around in circles while waiting for someone to die!")
+        if not stepper_thread.is_alive():
+            logger.error("The stepper process died unexpectedly! Oh no!")
+        if not streaming_thread.is_alive():
+            logger.error("The streaming process died unexpectedly! Oh no!")
+        if not imager_thread.is_alive():
+            logger.error("The imager process died unexpectedly! Oh no!")
+        time.sleep(1)
+
+    logger.info("Shutting down the shop")
+    stepper_thread.terminate()
+    streaming_thread.terminate()
+    imager_thread.terminate()
+    logger.info("Bye")
