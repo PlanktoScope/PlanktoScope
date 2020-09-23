6002012c2b7c314eb8a5005b463dfd82ce311043
Romain Bazile <gromain.baz@gmail.com>
2020-09-23 12:35:40 +0200

    imager: add state_machine class

---
+++ scripts/planktoscope/imager_state_machine.py
@@ -0,0 +1,63 @@
+# Logger library compatible with multiprocessing
+from loguru import logger
+
+# State machine class
+class ImagerState(object):
+    name = "state"
+    allowed = []
+
+    def switch(self, state):
+        """ Switch to new state """
+        if state.name in self.allowed:
+            logger.info(f"Current:{self} => switched to new state {state.name}")
+            self.__class__ = state
+        else:
+            logger.error(f"Current:{self} => switching to {state.name} not possible.")
+
+    def __str__(self):
+        return self.name
+
+
+class Stop(ImagerState):
+    name = "stop"
+    allowed = ["imaging"]
+
+
+class Imaging(ImagerState):
+    """ State of getting ready to start """
+
+    name = "imaging"
+    allowed = ["waiting"]
+
+
+class Waiting(ImagerState):
+    """ State of waiting for the pump to finish """
+
+    name = "waiting"
+    allowed = ["stop", "capture"]
+
+
+class Capture(ImagerState):
+    """ State of capturing image """
+
+    name = "capture"
+    allowed = ["stop", "waiting", "segmentation"]
+
+
+class Segmentation(ImagerState):
+    """ State of segmenting capteured images """
+
+    name = "segmentation"
+    allowed = ["stop"]
+
+
+class Imager(object):
+    """ A class representing the imager """
+
+    def __init__(self):
+        # State of the imager - default is stop.
+        self.state = Stop()
+
+    def change(self, state):
+        """ Change state """
+        self.state.switch(state)
