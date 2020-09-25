e687ed197887b837c4e1790ed39f5bfda976ad85
Romain Bazile <gromain.baz@gmail.com>
2020-09-25 12:49:41 +0200

    main: changes to accomodate the streamer/imager fusino

--- scripts/main.py
+++ scripts/main.py
@@ -60,8 +60,7 @@ def handler_stop_signals(signum, frame):
 
 if __name__ == "__main__":
     logger.info("Welcome!")
-
-    logger.info("Initialising signals handling (step 1/4)")
+    logger.info("Initialising signals handling (step 1/3)")
     signal.signal(signal.SIGINT, handler_stop_signals)
     signal.signal(signal.SIGTERM, handler_stop_signals)
 
@@ -70,25 +69,13 @@ if __name__ == "__main__":
     shutdown_event.clear()
 
     # Starts the stepper process for actuators
-    logger.info("Starting the stepper control process (step 2/4)")
+    logger.info("Starting the stepper control process (step 2/3)")
     stepper_thread = planktoscope.stepper.StepperProcess(shutdown_event)
     stepper_thread.start()
 
-    # Streaming server creation
-    output = planktoscope.streamer.StreamingOutput()
-    address = ("", 8000)
-    server = planktoscope.streamer.StreamingServer(
-        address, planktoscope.streamer.StreamingHandler
-    )
-    # Starts the streaming server process
-    logger.info("Starting the streaming server process (step 3/4)")
-    streaming_thread = multiprocessing.Process(target=server.serve_forever, daemon=True)
-    streaming_thread.start()
-
     # Starts the imager control process
-    logger.info("Starting the imager control process (step 4/4)")
+    logger.info("Starting the imager control process (step 3/3)")
     imager_thread = planktoscope.imager.ImagerProcess(shutdown_event)
-    imager_thread.start_camera(output)
     imager_thread.start()
 
     logger.info("Looks like everything is set up and running, have fun!")
@@ -99,15 +86,16 @@ if __name__ == "__main__":
         logger.trace("Running around in circles while waiting for someone to die!")
         if not stepper_thread.is_alive():
             logger.error("The stepper process died unexpectedly! Oh no!")
-        if not streaming_thread.is_alive():
-            logger.error("The streaming process died unexpectedly! Oh no!")
+            break
         if not imager_thread.is_alive():
             logger.error("The imager process died unexpectedly! Oh no!")
+            break
         time.sleep(1)
 
     logger.info("Shutting down the shop")
     shutdown_event.set()
     stepper_thread.join()
-    streaming_thread.terminate()
     imager_thread.join()
+    stepper_thread.close()
+    imager_thread.close()
     logger.info("Bye")
