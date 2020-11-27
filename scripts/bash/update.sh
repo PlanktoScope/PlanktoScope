f1b49e672b91542078d1aa619626cd1a6698122a
Romain Bazile <gromain.baz@gmail.com>
2020-11-27 11:53:21 +0100

    main.json: update to update code ;-)

--- scripts/bash/update.sh
+++ scripts/bash/update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+log="logger -t update.sh -s "
 
+${log} "Updating the main repository"
 cd /home/pi/PlanktonScope
 
 git stash
 git pull
 git checkout stash@{0} -- config.json hardware.json 
+${log} "Done!"
