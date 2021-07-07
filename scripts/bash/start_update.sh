ef09302e0dc830627ff7d13ee67289e49b909b16
Romain Bazile <romain@bazile.io>
2021-07-07 16:15:55 +0200

    update: temporary fix to cleanup things a bit

--- scripts/bash/start_update.sh
+++ scripts/bash/start_update.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 log="echo -e"
 
-${log} "Updating the installer script from $BRANCH"
-
+CURRENT_BRANCH=$(git --git-dir=/home/pi/PlanktoScope/.git rev-parse --abbrev-ref HEAD)
 REMOTE_BRANCHES=$(git --git-dir=/home/pi/PlanktoScope/.git branch --remotes --list | awk '/HEAD/{next;} split($1, a, "/") {print a[2]}')
 if [[ $# == 1 ]]; then
-    if [[ $REMOTE_BRANCHES =~ (^| )$1($| ) ]]; then
-        BRANCH=$1
+    if [[ $REMOTE_BRANCHES =~ (^|[[:space:]])$1($|[[:space:]]) ]]; then
+        BRANCH="$1"
     else
-        BRANCH="master"
+        BRANCH="$CURRENT_BRANCH"
     fi
 else
-    BRANCH="master"
+    BRANCH="$CURRENT_BRANCH"
 fi
-
+${log} "Updating the installer script from $BRANCH"
 curl "https://raw.githubusercontent.com/PlanktonPlanet/PlanktoScope/$BRANCH/scripts/bash/update.sh" > /tmp/update.sh
-
+chmod +x /tmp/update.sh
 exec /tmp/update.sh "$BRANCH"
