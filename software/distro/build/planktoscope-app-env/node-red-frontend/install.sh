38b64db57b0ad180f33b3bd3e629149e4b87b128
Ethan Li <lietk12@gmail.com>
2023-05-10 19:48:01 -0700

    Automate the manual configuration of Node-RED

--- software/distro/build/planktoscope-app-env/node-red-frontend/install.sh
+++ software/distro/build/planktoscope-app-env/node-red-frontend/install.sh
@@ -16,6 +16,12 @@ file="/home/pi/.node-red/settings.js"
 cp "$config_files_root$file" "$file"
 sudo chown 0:0 "$file"
 
+# Create default configs for the Node-RED editor
+file="/home/pi/.node-red/.config.projects.json"
+cp "$config_files_root$file" "$file"
+file="/home/pi/.node-red/.config.users.json"
+cp "$config_files_root$file" "$file"
+
 # Add systemd service modification to make Node-RED wait until Mosquitto has started
 # FIXME: The Node-RED frontend should instead be fixed so that it does not need to wait until
 # Mosquitto has started in order to work - until Mosquitto is up, the frontend should display a
