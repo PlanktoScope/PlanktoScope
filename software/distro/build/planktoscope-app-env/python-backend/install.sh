a690c3e684b908f9a890942e17adf497963433e6
Ethan Li <lietk12@gmail.com>
2023-05-11 18:14:05 -0700

    Try to use the pre-downloaded Git repo instead of cloning it from master

--- software/distro/build/planktoscope-app-env/python-backend/install.sh
+++ software/distro/build/planktoscope-app-env/python-backend/install.sh
@@ -23,31 +23,8 @@ cd /home/pi/libraries/WiringPi
 sudo /home/pi/libraries/WiringPi/build
 cd /home/pi
 
-# Get the list of Python dependencies
-mkdir -p /home/pi/PlanktoScope
-# FIXME: just instead use the repo which was downloaded to provide this shell script instead (will
-# require renaming the repo directory before running the top-level build script)
-git clone https://github.com/PlanktoScope/PlanktoScope /home/pi/PlanktoScope
-
-# Remove conflicting Python dependencies from the system
-sudo apt-get remove -y python3-numpy
-sudo apt-get autoremove -y
-
 # Install Python dependencies
 export PATH="/home/pi/.local/bin:$PATH"
-# FIXME: the following command is needed as a workaround for the lack of proper version locking of
-# pandas as an indirect dependency, and because for some reason pip can't find the pandas 2.0.1
-# pre-built binary from piwheels
-pip3 install pandas --prefer-binary
 # Note: the following command emits warnings about installed scripts not being in the user’s PATH,
 # but the installed scripts will be in the user’s path after reboot:
 pip3 install -U -r /home/pi/PlanktoScope/requirements.txt
-# Upgrade adafruit dependencies
-# FIXME: just use the requirements.txt file instead
-pip3 install --upgrade adafruit-blinka adafruit-platformdetect
-# Fix numpy version
-# FIXME: just use the requirements.txt file instead. We need to install numpy v1.22.4 rather than
-# v1.21.4 because scipy v1.7.2 has a binary incompatibility with numpy 1.21.4 when both are installed
-# on our system via pip with piwheels, even though those two versions are supposed to be compatible.
-pip3 uninstall -y numpy
-pip3 install numpy==1.22.4
