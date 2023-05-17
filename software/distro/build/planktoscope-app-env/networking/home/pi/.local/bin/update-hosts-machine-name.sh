1ca297cc0546cb5d67b8774d451ed322674b2f12
Ethan Li <lietk12@gmail.com>
2023-05-17 00:18:26 -0700

    Fix typoes in scripts

--- software/distro/build/planktoscope-app-env/networking/home/pi/.local/bin/update-hosts-machine-name.sh
+++ software/distro/build/planktoscope-app-env/networking/home/pi/.local/bin/update-hosts-machine-name.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 SERIAL_NUMBER="$(tr -d '\0' < /sys/firmware/devicetree/base/serial-number | cut -c 9-)"
-MACHINE_NAME="$(/home/pi/.local/bin/machine-name name —format=hex —sn="$SERIAL_NUMBER")"
+MACHINE_NAME="$(/home/pi/.local/bin/machine-name name --format=hex --sn="$SERIAL_NUMBER")"
 cp /home/pi/.local/etc/hosts-base.snippet /home/pi/.local/etc/hosts
 sed "s/machine-name/$MACHINE_NAME/g" /home/pi/.local/etc/hosts-machine-name.snippet \
   >> /home/pi/.local/etc/hosts
