851a030ba4732645a3e4c8cb0cf8e0739995c374
Ethan Li <lietk12@gmail.com>
2023-05-26 23:14:39 -0700

    Fix/work around some issues with the setup script for the Forklift env

--- software/distro/build/planktoscope-app-env/forklift/install.sh
+++ software/distro/build/planktoscope-app-env/forklift/install.sh
@@ -3,7 +3,11 @@
 # Docker containerized applications for the PlanktoScope software distribution
 
 curl -L https://github.com/PlanktoScope/forklift/releases/download/v0.1.5/forklift_0.1.5_linux_arm.tar.gz | tar -C /home/pi/.local/bin -xz forklift
-forklift --workspace /home/pi/.forklift env clone github.com/PlanktoScope/pallets-env@cdbe27e
-forklift --workspace /home/pi/.forklift env cache
-# TODO: maybe we should pre-cache the Docker container images and instead deploy on first boot?
-sudo -E /home/pi/.local/bin/forklift --workspace /home/pi/.forklift env deploy
+/home/pi/.local/bin/forklift --workspace /home/pi/.forklift env clone github.com/PlanktoScope/pallets-env@cdbe27e
+# FIXME: we need to pre-cache the Docker container images so that we don't need internet when we deploy the packages. Or we should just use Docker Compose.
+/home/pi/.local/bin/forklift --workspace /home/pi/.forklift env cache
+
+# Deploy environment after Docker Swarm is initialized
+file="/etc/systemd/system/first-boot-forklift-deploy.service"
+sudo cp "$config_files_root$file" "$file"
+sudo systemctl enable first-boot-forklift-deploy.service
