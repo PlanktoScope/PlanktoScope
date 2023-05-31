bff8dfd3f28b199c9c1b978106513b5abff31c05
Ethan Li <lietk12@gmail.com>
2023-05-30 20:50:59 -0700

    Fix error with trying to cache Docker container images

--- software/distro/build/planktoscope-app-env/forklift/install.sh
+++ software/distro/build/planktoscope-app-env/forklift/install.sh
@@ -5,7 +5,7 @@
 curl -L https://github.com/PlanktoScope/forklift/releases/download/v0.1.6/forklift_0.1.6_linux_arm.tar.gz | tar -C /home/pi/.local/bin -xz forklift
 /home/pi/.local/bin/forklift --workspace /home/pi/.forklift env clone github.com/PlanktoScope/pallets-env@5fb6f83
 /home/pi/.local/bin/forklift --workspace /home/pi/.forklift env cache-repo
-/home/pi/.local/bin/forklift --workspace /home/pi/.forklift env cache-img
+sudo -E /home/pi/.local/bin/forklift --workspace /home/pi/.forklift env cache-img
 
 # Deploy environment after Docker Swarm is initialized
 file="/etc/systemd/system/first-boot-forklift-deploy.service"
