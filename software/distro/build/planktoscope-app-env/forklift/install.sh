ac49561f5c0c6b69641846f0d0c5ab56c9533996
Ethan Li <lietk12@gmail.com>
2023-05-30 20:56:20 -0700

    Fix missing variable in the Forklift environment install script

--- software/distro/build/planktoscope-app-env/forklift/install.sh
+++ software/distro/build/planktoscope-app-env/forklift/install.sh
@@ -2,6 +2,8 @@
 # The Forklift environment provides the standard configuration of Pallet package deployments of
 # Docker containerized applications for the PlanktoScope software distribution
 
+config_files_root=$(dirname $(realpath $BASH_SOURCE))
+
 curl -L https://github.com/PlanktoScope/forklift/releases/download/v0.1.6/forklift_0.1.6_linux_arm.tar.gz | tar -C /home/pi/.local/bin -xz forklift
 /home/pi/.local/bin/forklift --workspace /home/pi/.forklift env clone github.com/PlanktoScope/pallets-env@5fb6f83
 /home/pi/.local/bin/forklift --workspace /home/pi/.forklift env cache-repo
