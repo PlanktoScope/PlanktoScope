bc7c876ce7be3497704bf82b85265ea47faeda4e
Ethan Li <lietk12@gmail.com>
2023-06-01 16:42:36 -0700

    Add a clarification in code comments

--- software/distro/build/base-os/networking/home/pi/.local/bin/enable-interface-forwarding.sh
+++ software/distro/build/base-os/networking/home/pi/.local/bin/enable-interface-forwarding.sh
@@ -8,6 +8,6 @@ iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 iptables -A FORWARD -i wlan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
 
-# Redirect traffic bound for 192.168.4.1 and 192.168.5.1 to 127.0.0.1
+# Redirect external incoming traffic bound for 192.168.4.1 and 192.168.5.1 to 127.0.0.1
 iptables -t nat -A PREROUTING -i eth0 -d 192.168.4.1 -j DNAT --to-destination 127.0.0.1
 iptables -t nat -A PREROUTING -i wlan0 -d 192.168.5.1 -j DNAT --to-destination 127.0.0.1
