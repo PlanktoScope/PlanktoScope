fbfa77f186072b7fb4d7cd153922439b148c9247
Ethan Li <lietk12@gmail.com>
2023-05-31 17:24:10 -0700

    Make first-boot services run correctly without internet access

--- software/distro/build/planktoscope-app-env/forklift/install.sh
+++ software/distro/build/planktoscope-app-env/forklift/install.sh
@@ -4,7 +4,7 @@
 
 config_files_root=$(dirname $(realpath $BASH_SOURCE))
 
-curl -L https://github.com/PlanktoScope/forklift/releases/download/v0.1.6/forklift_0.1.6_linux_arm.tar.gz | tar -C /home/pi/.local/bin -xz forklift
+curl -L https://github.com/PlanktoScope/forklift/releases/download/v0.1.7/forklift_0.1.7_linux_arm.tar.gz | tar -C /home/pi/.local/bin -xz forklift
 /home/pi/.local/bin/forklift --workspace /home/pi/.forklift env clone github.com/PlanktoScope/pallets-env@5fb6f83
 /home/pi/.local/bin/forklift --workspace /home/pi/.forklift env cache-repo
 sudo -E /home/pi/.local/bin/forklift --workspace /home/pi/.forklift env cache-img
