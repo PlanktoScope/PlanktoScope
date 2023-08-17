16821e18ab7b84525a3948781636807443bd7bdb
Ethan Li <lietk12@gmail.com>
2023-08-17 16:07:53 -0700

    Try setting up the backend from github.com/PlanktoScope/device-backend

--- software/distro/build/planktoscope-app-env/python-backend/install.sh
+++ software/distro/build/planktoscope-app-env/python-backend/install.sh
@@ -11,8 +11,12 @@ sudo apt-get update -y
 # dependencies via apt-get?
 sudo apt-get install -y git python3-pip libatlas3-base \
   libhdf5-103-1 libopenjp2-7 libopenexr25 libavcodec58 libavformat58 libswscale5
+python3 -m pip install --user pipx==1.2.0
+python3 -m pipx ensurepath
+pipx install poetry==1.5.1
 
 # Install Fan HAT dependencies
+# FIXME: do we even need to do this build?
 sudo apt install -y i2c-tools
 mkdir -p /home/pi/libraries
 # FIXME: can we get a reproducible build of WiringPi? Or maybe just Python library support via pip?
@@ -24,6 +28,11 @@ cd /home/pi
 
 # Install Python dependencies
 export PATH="/home/pi/.local/bin:$PATH"
-# Note: the following command emits warnings about installed scripts not being in the user’s PATH,
-# but the installed scripts will be in the user’s path after reboot:
-pip3 install -U -r /home/pi/PlanktoScope/requirements.txt
+# TODO: pin this at a specific version (at least until we distribute it as a Docker image)
+wget https://github.com/PlanktoScope/device-backend/archive/refs/heads/main.zip
+unzip main.zip
+rm main.zip
+mv device-backend-main /home/pi/device-backend
+cd /home/pi/device-backend
+poetry init
+cd -
