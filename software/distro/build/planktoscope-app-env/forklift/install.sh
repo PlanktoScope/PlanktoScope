1729a66bcd7561bcaea5a9443561f8eac46dc2ca
Ethan Li <lietk12@gmail.com>
2023-08-23 21:52:32 -0700

    Apply pallet during install (don't need to wait until first boot)

--- software/distro/build/planktoscope-app-env/forklift/install.sh
+++ software/distro/build/planktoscope-app-env/forklift/install.sh
@@ -5,18 +5,11 @@
 
 config_files_root=$(dirname $(realpath $BASH_SOURCE))
 forklift_version="0.3.1"
-pallet_version="ac32c3b"
+pallet_version="44f7c59"
 
 curl -L "https://github.com/PlanktoScope/forklift/releases/download/v$forklift_version/forklift_${forklift_version}_linux_arm.tar.gz" \
   | tar -C /home/pi/.local/bin -xz forklift
 /home/pi/.local/bin/forklift --workspace /home/pi/.forklift plt clone github.com/PlanktoScope/pallet-standard@$pallet_version
 /home/pi/.local/bin/forklift --workspace /home/pi/.forklift plt cache-repo
 sudo -E /home/pi/.local/bin/forklift --workspace /home/pi/.forklift plt cache-img
-# Note: we don't apply the pallet immediately because the Docker service won't work until a reboot.
-
-# Apply pallet after Docker is initialized, because the system needs to be restarted after Docker is
-# installed before the Docker service will be able to start successfully.
-# Refer to https://www.reddit.com/r/raspberry_pi/comments/zblky6/comment/iytpp4g/ for details.
-file="/etc/systemd/system/first-boot-forklift-apply.service"
-sudo cp "$config_files_root$file" "$file"
-sudo systemctl enable first-boot-forklift-apply.service
+sudo -E /home/pi/.local/bin/forklift --workspace /home/pi/.forklift plt apply
