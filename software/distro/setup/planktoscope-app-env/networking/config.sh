37e40a93681fa15c68326a5ca0d417e71cd97bdf
Ethan Li <lietk12@gmail.com>
2023-09-06 20:47:41 -0700

    Limit SSID length for wifi hotspot networks (#214)
    * Shorten SSIDs & domain names; add comments on autogenerated files/lines
    
    * Bump docs dependencies
    
    * Bump pallet to upgrade device-portal & docs
    
    * Try to escape the hostapd ssid warning comment for sed
    
    * Make a missing directory in the setup script
    
    * Fix update scripts for machine-name; copy missing file in setup
    
    * Fix incorrect file path in setup script
    
    * Automatically make `/home/pi/.local/etc/machine-name` with machine name
    
    * Remove references to a file which actually doesn't exist (oops)
    
    * Fix incorrect path
    
    * Update changelog
    
    * Fix misplaced setup command leading to a missing file
    
    * Fix typo in sed expression

--- software/distro/setup/planktoscope-app-env/networking/config.sh
+++ software/distro/setup/planktoscope-app-env/networking/config.sh
@@ -10,19 +10,37 @@ sudo apt-get install -y avahi-utils
 
 # Set the default hostname, which will be updated with the machine name on boot
 current_hostname=$(hostnamectl status --static)
-new_hostname="planktoscope"
+new_hostname="pkscope"
 sudo bash -c "echo \"$new_hostname\" > /etc/hostname"
 sudo sed -i "s/^127\.0\.1\.1.*$current_hostname$/127.0.1.1\t$new_hostname/g" /etc/hosts
 sudo hostnamectl set-hostname "$new_hostname"
 
 # Set the default SSID for the self-hosted wifi network, which will be updated with the machine name on boot
-sudo sed -i "s/^ssid=.*$/ssid=PlanktoScope/g" /etc/hostapd/hostapd.conf
+file="/etc/hostapd/hostapd-ssid-autogen-warning.snippet"
+# This sed command uses `~` instead of `/` because the warning comments also include `/` characters.
+# The awk subcommand is needed to escape newlines for sed.
+sudo sed -i "s~^ssid=.*$~$(awk '{printf "%s\\n", $0}' $file)ssid=pkscope~g" /etc/hostapd/hostapd.conf
 
 # Download tool to generate machine names based on serial numbers
-curl -L https://github.com/PlanktoScope/machine-name/releases/download/v0.1.2/machine-name_0.1.2_linux_arm.tar.gz \
+machinename_version="0.1.3"
+curl -L "https://github.com/PlanktoScope/machine-name/releases/download/v$machinename_version/machine-name_${machinename_version}_linux_arm.tar.gz" \
   | tar -xz -C /home/pi/.local/bin/ machine-name
 
-# Automatically update the SSID upon creation of the self-hosted wifi network based on the RPi's serial number
+# Automatically generate the machine name and write it to a file upon boot
+mkdir -p /home/pi/.local/etc
+file="/home/pi/.local/etc/machine-name"
+cp "$config_files_root$file" "$file"
+mkdir -p /home/pi/.local/bin
+file="/home/pi/.local/bin/update-machine-name.sh"
+cp "$config_files_root$file" "$file"
+file="/etc/systemd/system/planktoscope-org.update-machine-name.service"
+sudo cp "$config_files_root$file" "$file"
+sudo systemctl enable planktoscope-org.update-machine-name.service
+
+# Automatically update the SSID upon creation of the self-hosted wifi network based on the machine name
+mkdir -p /home/pi/.local/etc/hostapd
+file="/home/pi/.local/etc/hostapd/ssid.snippet"
+cp "$config_files_root$file" "$file"
 mkdir -p /home/pi/.local/bin
 file="/home/pi/.local/bin/update-ssid-machine-name.sh"
 cp "$config_files_root$file" "$file"
@@ -36,8 +54,6 @@ sudo bash -c "cat \"$config_files_root$file.snippet\" >> \"$file\""
 
 # Automatically update the Cockpit origins upon boot with the machine name
 mkdir -p /home/pi/.local/etc/cockpit
-file="/home/pi/.local/etc/cockpit/origins"
-cp "$config_files_root$file" "$file"
 file="/home/pi/.local/etc/cockpit/origins-base.snippet"
 cp "$config_files_root$file" "$file"
 file="/home/pi/.local/etc/cockpit/origins-machine-name.snippet"
@@ -53,21 +69,30 @@ sudo systemctl enable planktoscope-org.update-cockpit-origins-machine-name.servi
 file="/etc/dnsmasq.d/planktoscope.conf"
 sudo cp "$config_files_root$file" "$file"
 mkdir -p /home/pi/.local/etc
-file="/home/pi/.local/etc/hosts"
+file="/home/pi/.local/etc/hosts-autogen-warning.snippet"
 cp "$config_files_root$file" "$file"
-
-# Automatically update hostnames upon boot with the machine name
 file="/home/pi/.local/etc/hosts-base.snippet"
 cp "$config_files_root$file" "$file"
+cp "/home/pi/.local/etc/hosts-autogen-warning.snippet" \
+  "/home/pi/.local/etc/hosts"
+cat "/home/pi/.local/etc/hosts-base.snippet" \
+  >> "/home/pi/.local/etc/hosts"
+
+# Automatically update hostnames upon boot with the machine name
+mkdir -p /home/pi/.local/etc
 file="/home/pi/.local/etc/hosts-machine-name.snippet"
 cp "$config_files_root$file" "$file"
+mkdir -p /home/pi/.local/bin
 file="/home/pi/.local/bin/update-hosts-machine-name.sh"
 cp "$config_files_root$file" "$file"
 file="/etc/systemd/system/planktoscope-org.update-hosts-machine-name.service"
 sudo cp "$config_files_root$file" "$file"
 sudo systemctl enable planktoscope-org.update-hosts-machine-name.service
 
-# Publish planktoscope.local-based aliases
+# Publish planktoscope.local and pkscope.local mDNS aliases
 file="/etc/systemd/system/planktoscope-org.avahi-alias-planktoscope.local.service"
 sudo cp "$config_files_root$file" "$file"
 sudo systemctl enable planktoscope-org.avahi-alias-planktoscope.local.service
+file="/etc/systemd/system/planktoscope-org.avahi-alias-pkscope.local.service"
+sudo cp "$config_files_root$file" "$file"
+sudo systemctl enable planktoscope-org.avahi-alias-pkscope.local.service
