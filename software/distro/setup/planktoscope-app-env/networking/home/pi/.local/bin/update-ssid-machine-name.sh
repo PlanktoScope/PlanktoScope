15aea9441da3050b71f762498a4d1a4378ef7363
Ethan Li <lietk12@gmail.com>
2023-09-06 20:26:22 -0700

    Fix incorrect path

--- software/distro/setup/planktoscope-app-env/networking/home/pi/.local/bin/update-ssid-machine-name.sh
+++ software/distro/setup/planktoscope-app-env/networking/home/pi/.local/bin/update-ssid-machine-name.sh
@@ -3,5 +3,5 @@
 serial_number="$(tr -d '\0' < /sys/firmware/devicetree/base/serial-number | cut -c 9-)"
 machine_name="$(/home/pi/.local/bin/machine-name name --format=hex --sn="$serial_number")"
 
-ssid="$(sed "s/{machine-name}/$machine_name/g" /home/pi/etc/hostapd/ssid.snippet | sed 's/#.*$//g' | sed '^$/d')"
+ssid="$(sed "s/{machine-name}/$machine_name/g" /home/pi/.local/etc/hostapd/ssid.snippet | sed 's/#.*$//g' | sed '^$/d')"
 sed -i "s/^ssid=.*$/ssid=$ssid/g" /etc/hostapd/hostapd.conf
