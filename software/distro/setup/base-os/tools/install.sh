0065decbaa53e12fa5a994dd1fe0ab1f6b780a48
Ethan Li <lietk12@gmail.com>
2023-12-31 10:44:07 -0800

    Allow Docker to start before `network-online.target`

--- software/distro/setup/base-os/tools/install.sh
+++ software/distro/setup/base-os/tools/install.sh
@@ -41,6 +41,11 @@ sudo apt-get install -y \
   docker-compose-plugin=$COMPOSE_VERSION_STRING
 sudo apt-get remove -y docker-buildx-plugin
 
+# Allow Docker to start before the device is online on a network
+file="/etc/systemd/system/docker.service.d/override.conf"
+sudo cp "$config_files_root$file" "$file"
+sudo systemctl daemon-reload
+
 # Install cockpit
 sudo apt-get update -y
 sudo apt-get install -y --no-install-recommends cockpit
