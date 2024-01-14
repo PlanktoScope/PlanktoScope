6b9d385dd982cd7bd3c0701d47c2912113a928fe
Ethan Li <lietk12@gmail.com>
2024-01-14 09:07:13 -0800

    Move some scripts over into management by forklift

--- software/distro/setup/planktoscope-app-env/forklift/install.sh
+++ software/distro/setup/planktoscope-app-env/forklift/install.sh
@@ -5,7 +5,7 @@
 
 config_files_root=$(dirname $(realpath $BASH_SOURCE))
 forklift_version="0.5.0"
-pallet_version="c84c827"
+pallet_version="fbf442db"
 
 curl -L "https://github.com/PlanktoScope/forklift/releases/download/v$forklift_version/forklift_${forklift_version}_linux_arm.tar.gz" \
   | tar -C $HOME/.local/bin -xz forklift
--- software/distro/setup/planktoscope-app-env/networking/home/pi/.local/bin/update-machine-name.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-
-serial_number="$(tr -d '\0' < /sys/firmware/devicetree/base/serial-number | cut -c 9-)"
-machine_name="$(/home/pi/.local/bin/machine-name name --format=hex --sn="$serial_number")"
-
-# Update /home/pi/.local/etc/machine-name
-echo "$machine_name" > /home/pi/.local/etc/machine-name
