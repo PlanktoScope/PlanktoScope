fec6ae587d4fe7ed10fa1e4530a52a0a32036edd
Ethan Li <lietk12@gmail.com>
2024-01-14 09:54:34 -0800

    Download forklift binary to `/usr/bin` instead of `$HOME/.local/bin`

--- software/distro/setup/base-os/forklift/install.sh
+++ software/distro/setup/base-os/forklift/install.sh
@@ -9,16 +9,15 @@ pallet_path="github.com/PlanktoScope/pallet-standard"
 pallet_version="fbf442db"
 
 curl -L "https://github.com/PlanktoScope/forklift/releases/download/v$forklift_version/forklift_${forklift_version}_linux_arm.tar.gz" \
-  | tar -C $HOME/.local/bin -xz forklift
+  | tar -C /usr/bin -xz forklift
 workspace="$HOME"
-forklift="$HOME/.local/bin/forklift --workspace $workspace"
-$forklift plt clone --force $pallet_path@$pallet_version
-$forklift plt cache-repo
+forklift --workspace $HOME plt clone --force $pallet_path@$pallet_version
+forklift --workspace $HOME plt cache-repo
 
 # Note: this command must run with sudo even though the pi user has been added to the docker
 # usergroup, because that change only works after starting a new login shell; and `newgrp docker`
 # doesn't work either.
-sudo -E $forklift plt cache-img --parallel # to save disk space, we don't cache images used by disabled package deployments
+sudo -E forklift --workspace $HOME plt cache-img --parallel # to save disk space, we don't cache images used by disabled package deployments
 
 # The pallet must be applied during each startup because we're using Docker Compose, not Swarm Mode.
 file="/etc/systemd/system/forklift-apply.service"
