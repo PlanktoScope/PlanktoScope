85b78f40dd001ae11e4983656ca1eb95cbba7a38
Ethan Li <lietk12@gmail.com>
2024-01-14 00:52:42 -0800

    Run `forklift plt apply` with parallelization (#348)
    * Run `forklift plt apply` with parallelization
    
    * Update `CHANGELOG.md`
    
    * Fix incorrect workspace, try skipping `plt apply` in setup process
    
    * Remove commented-out code

--- software/distro/setup/planktoscope-app-env/forklift/install.sh
+++ software/distro/setup/planktoscope-app-env/forklift/install.sh
@@ -4,40 +4,22 @@
 # distribution
 
 config_files_root=$(dirname $(realpath $BASH_SOURCE))
-forklift_version="0.4.0"
-pallet_version="72ecce01"
+forklift_version="0.5.0"
+pallet_version="c84c827"
 
 curl -L "https://github.com/PlanktoScope/forklift/releases/download/v$forklift_version/forklift_${forklift_version}_linux_arm.tar.gz" \
   | tar -C $HOME/.local/bin -xz forklift
-workspace="$HOME/.forklift"
+workspace="$HOME"
 forklift="$HOME/.local/bin/forklift --workspace $workspace"
-if [ -d "$workspace" ]; then
-  $forklift plt rm
-fi
-$forklift plt clone github.com/PlanktoScope/pallet-standard@$pallet_version
+$forklift plt clone --force github.com/PlanktoScope/pallet-standard@$pallet_version
 $forklift plt cache-repo
 
-# Note: the below commands must run with sudo even though the pi user has been added to the docker
+# Note: this command must run with sudo even though the pi user has been added to the docker
 # usergroup, because that change only works after starting a new login shell; and `newgrp docker`
 # doesn't work either.
-# Note: cache-img downloads images even for disabled package deployments. We skip it to save disk
-# space (because the node-red container image used by a test package is >100 MB, even though we
-# don't need it yet), we don't yet have any packages in the pallet which someone might want to
-# enable, and because we're running plt apply anyways - that command will download images as needed
-# for each (enabled) package deployment. We will want to have a cache-img flag which controls
-# whether only enabled package deployments are cached or all package deployments are cached, so that
-# we can use the command here to only cache enabled package deployments. We may get a slight speedup
-# if we can download all the images in parallel, without having to start the services all in
-# parallel (which would add lots of runtime nondeterminism to the system)
-# sudo -E $forklift plt cache-img
-sudo -E $forklift plt apply
-# Note: we apply the pallet immediately so that the first boot of the image won't be excessively
-# slow (due to Docker Compose needing to create all the services from scratch rather than simply
-# starting them).
+sudo -E $forklift plt cache-img --parallel # to save disk space, we don't cache images used by disabled package deployments
 
-# Apply pallet after Docker is initialized, because the system needs to be restarted after Docker is
-# installed before the Docker service will be able to start successfully.
-# Refer to https://www.reddit.com/r/raspberry_pi/comments/zblky6/comment/iytpp4g/ for details.
+# The pallet must be applied during each startup because we're using Docker Compose, not Swarm Mode.
 file="/etc/systemd/system/planktoscope-org.forklift-apply.service"
 sudo cp "$config_files_root$file" "$file"
 sudo systemctl enable planktoscope-org.forklift-apply.service
