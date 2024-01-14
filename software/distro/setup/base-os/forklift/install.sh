7e5a8584af2e504e8844c437d53ba4457a9638f3
Ethan Li <lietk12@gmail.com>
2024-01-14 15:30:57 -0800

    Change location of machine-name file

--- software/distro/setup/base-os/forklift/install.sh
+++ software/distro/setup/base-os/forklift/install.sh
@@ -6,7 +6,7 @@
 config_files_root=$(dirname $(realpath $BASH_SOURCE))
 forklift_version="0.5.0"
 pallet_path="github.com/PlanktoScope/pallet-standard"
-pallet_version="6a3d8db6"
+pallet_version="f6a71706"
 
 curl -L "https://github.com/PlanktoScope/forklift/releases/download/v$forklift_version/forklift_${forklift_version}_linux_arm.tar.gz" \
   | sudo tar -C /usr/bin -xz forklift
--- software/distro/setup/planktoscope-app-env/python-backend/install.sh
+++ software/distro/setup/planktoscope-app-env/python-backend/install.sh
@@ -28,7 +28,7 @@ $POETRY_VENV/bin/pip install cryptography==41.0.5
 $POETRY_VENV/bin/pip install poetry==1.7.1
 
 # Download device-backend monorepo
-backend_version="2872828" # this should be either a version tag, branch name, or commit hash
+backend_version="419413d" # this should be either a version tag, branch name, or commit hash
 git clone https://github.com/PlanktoScope/device-backend $HOME/device-backend --no-checkout --filter=blob:none
 git -C $HOME/device-backend checkout --quiet $backend_version
 
