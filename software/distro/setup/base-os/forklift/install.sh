eac98c252d2c9cf7f03b73893869c7f3e0586e9d
Ethan Li <lietk12@gmail.com>
2024-01-17 00:36:42 -0800

    Bump pallet-standard

--- software/distro/setup/base-os/forklift/install.sh
+++ software/distro/setup/base-os/forklift/install.sh
@@ -6,7 +6,7 @@
 config_files_root=$(dirname $(realpath $BASH_SOURCE))
 forklift_version="0.5.0"
 pallet_path="github.com/PlanktoScope/pallet-standard"
-pallet_version="6483eae"
+pallet_version="dbe3daf5"
 
 curl -L "https://github.com/PlanktoScope/forklift/releases/download/v$forklift_version/forklift_${forklift_version}_linux_arm.tar.gz" \
   | sudo tar -C /usr/bin -xz forklift
