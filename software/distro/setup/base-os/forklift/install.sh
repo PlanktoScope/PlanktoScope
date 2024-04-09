f29417e70d84bc6212957e36604f78dc022b7964
Ethan Li <lietk12@gmail.com>
2024-04-09 16:32:51 -0700

    Fix error in previous commit

--- software/distro/setup/base-os/forklift/install.sh
+++ software/distro/setup/base-os/forklift/install.sh
@@ -43,7 +43,7 @@ sudo sh -c "\
   mkdir /etc && \
   printf 'uninitialized\n' > /etc/machine-id && \
   cp /usr/etc/fstab /etc/fstab && \
-  cp -r /usr/etc/systemd/*.wants /etc/systemd && \
+  cp -r /usr/etc/systemd/system/*.wants /etc/systemd && \
   mount --bind /usr/etc /etc"
 sudo systemctl daemon-reload
 
