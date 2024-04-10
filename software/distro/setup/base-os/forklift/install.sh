97675180e3af24dae2efb9c02a817a643ab11dbb
Ethan Li <lietk12@gmail.com>
2024-04-09 18:38:25 -0700

    Try to fix failure to initialize `/etc/machine-id`

--- software/distro/setup/base-os/forklift/install.sh
+++ software/distro/setup/base-os/forklift/install.sh
@@ -21,7 +21,7 @@ sudo ln -s "/usr/bin/forklift-${forklift_version}" /usr/bin/forklift
 
 workspace="$HOME"
 forklift --workspace $workspace plt clone --force $pallet_path@$pallet_version
-forklift --workspace $workspace plt cache-repo
+#forklift --workspace $workspace plt cache-repo
 
 # Note: this command must run with sudo even though the pi user has been added to the docker
 # usergroup, because that change only works after starting a new login shell; and `newgrp docker`
@@ -49,12 +49,18 @@ sudo sh -c "\
 sudo systemctl daemon-reload
 
 # Set up overlay for /etc
+# We enable these systemd units directly via symlink because `systemctl enable` makes the symlink in
+# `/etc`, which we've bind-mounted to `/usr/etc` (so it won't show up at boot in `/etc` before the
+# mount is started - and the purpose of these units is to use the overlay for `/etc`):
+file="/usr/lib/systemd/system/bind-etc-machine-id.service"
+sudo cp "$config_files_root$file" "$file"
 file="/usr/lib/systemd/system/etc.mount"
 sudo cp "$config_files_root$file" "$file"
-# We enable the mount directly via symlink because `systemctl enable` makes the symlink in `/etc`,
-# which we've bind-mounted to `/usr/etc` (so it won't show up at boot in `/etc` before the mount is
-# started - and the purpose of the mount is to use `/usr/etc` for `/etc`):
 sudo ln -s "$file" /usr/lib/systemd/system/local-fs.target.wants/etc.mount
+file="/usr/lib/systemd/system/etc-mounted-daemon-reload.mount"
+sudo cp "$config_files_root$file" "$file"
+sudo ln -s "$file" /usr/lib/systemd/system/basic.target.wants/etc-mounted-daemon-reload.mount
 sudo mkdir -p /var/lib/forklift/exports/next/overlays/etc
+sudo mkdir -p /var/lib/planktoscope/generated/etc
 sudo mkdir -p /var/lib/overlays/overrides/etc
 sudo mkdir -p /var/lib/overlays/workdirs/etc
