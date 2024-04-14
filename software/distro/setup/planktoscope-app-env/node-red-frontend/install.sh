bca5b11532a9eabe6053233f4aef60714d2cc117
Ethan Li <lietk12@gmail.com>
2024-04-14 15:43:08 -0700

    Try installing the Node-RED dashboard's smbus dependency in a bookworm-compatible way

--- software/distro/setup/planktoscope-app-env/node-red-frontend/install.sh
+++ software/distro/setup/planktoscope-app-env/node-red-frontend/install.sh
@@ -19,8 +19,7 @@ fi
 # default, we do `pip3 install` as the pi user. This makes the smbus2 module available to Node-RED.
 # FIXME: get rid of the Node-RED nodes depending on smbus! That functionality should be moved into
 # the Python backend.
-sudo apt-get install -y python3-pip
-pip3 install smbus2==0.4.3
+sudo apt-get install -y python3-smbus2
 
 # Install Node-RED
 # TODO: run Node-RED in a Docker container instead
