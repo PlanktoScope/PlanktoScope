f9269e4588fcfcc74fe10cffd963a2af01ada06c
Ethan Li <lietk12@gmail.com>
2024-04-13 19:13:33 -0700

    Fix error in previous commit

--- software/distro/setup/base-os/forklift/install.sh
+++ software/distro/setup/base-os/forklift/install.sh
@@ -37,7 +37,7 @@ sudo systemctl preset forklift-apply.service
 # Set up read-write filesystem overlays with forklift-managed layers for /etc and /usr
 # (see https://docs.kernel.org/filesystems/overlayfs.html):
 sudo systemctl preset \
-  bindro-sysroot.service.service \
+  bindro-sysroot.service \
   overlay-run-forklift-stages-current.service \
   overlay-usr.service.service \
   overlay-etc.service.service \
