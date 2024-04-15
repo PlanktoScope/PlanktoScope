790fbd9ba05f9fe26c5bbfbf4ffb8279296b8da7
Ethan Li <lietk12@gmail.com>
2024-04-14 18:16:30 -0700

    Try to prevent error in cleanup script for the "segmenter-only" hardware type

--- software/distro/setup/planktoscope-app-env/cleanup.sh
+++ software/distro/setup/planktoscope-app-env/cleanup.sh
@@ -8,12 +8,14 @@ sudo apt-get clean -y
 # Clean up any unnecessary pip, poetry, and npm files
 pip3 cache purge || true
 POETRY_VENV=$HOME/.local/share/pypoetry/venv
-BACKEND_CONTROLLER=$HOME/device-backend/control
-$POETRY_VENV/bin/poetry --no-interaction --directory $BACKEND_CONTROLLER cache clear _default_cache --all
-$POETRY_VENV/bin/poetry --no-interaction --directory $BACKEND_CONTROLLER cache clear piwheels --all
-BACKEND_PROCESSING_SEGMENTER=$HOME/device-backend/processing/segmenter
-$POETRY_VENV/bin/poetry --no-interaction --directory $BACKEND_PROCESSING_SEGMENTER cache clear _default_cache --all
-$POETRY_VENV/bin/poetry --no-interaction --directory $BACKEND_PROCESSING_SEGMENTER cache clear piwheels --all
+if [ -f $POETRY_VENV/bin/poetry ]; then
+  BACKEND_CONTROLLER=$HOME/device-backend/control
+  $POETRY_VENV/bin/poetry --no-interaction --directory $BACKEND_CONTROLLER cache clear _default_cache --all
+  $POETRY_VENV/bin/poetry --no-interaction --directory $BACKEND_CONTROLLER cache clear piwheels --all
+  BACKEND_PROCESSING_SEGMENTER=$HOME/device-backend/processing/segmenter
+  $POETRY_VENV/bin/poetry --no-interaction --directory $BACKEND_PROCESSING_SEGMENTER cache clear _default_cache --all
+  $POETRY_VENV/bin/poetry --no-interaction --directory $BACKEND_PROCESSING_SEGMENTER cache clear piwheels --all
+fi
 
 # Remove history files
 rm -f $HOME/.python_history
