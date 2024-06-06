9deaa584de8babd54cd7f98d8028782877cd36c9
Ethan Li <lietk12@gmail.com>
2024-06-06 16:50:29 -0700

    Fix docker permissions issue in non-CI setup

--- software/distro/setup/base-os/forklift/transfer-precached-image.sh
+++ software/distro/setup/base-os/forklift/transfer-precached-image.sh
@@ -4,5 +4,5 @@ image="$1"
 precached_image="$HOME/.cache/containers/$(echo "$image" | sed "s~:~;~").tar"
 
 echo "Loading $image from $precached_image..."
-docker image load --quiet < "$precached_image"
+sudo docker image load --quiet < "$precached_image"
 rm "$precached_image"
