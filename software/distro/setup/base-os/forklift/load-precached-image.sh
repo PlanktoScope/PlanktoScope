ec8687188ecdfd91abc10746605efa3835fb68c1
Ethan Li <lietk12@gmail.com>
2024-06-14 13:02:39 -0700

    Try to load container images into containerd without QEMU VM

--- software/distro/setup/base-os/forklift/load-precached-image.sh
+++ software/distro/setup/base-os/forklift/load-precached-image.sh
@@ -5,5 +5,5 @@ cache_path="$2" # e.g. ~/.cache/containers
 precached_image="$cache_path/$image.tar"
 
 echo "Loading $image from $precached_image..."
-sudo docker image load --quiet < "$precached_image"
+sudo nerdctl image load --quiet < "$precached_image"
 rm "$precached_image"
