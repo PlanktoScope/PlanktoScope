6306f7f119b66541861c1222754493658bed27e9
Ethan Li <lietk12@gmail.com>
2024-06-15 09:55:54 -0700

    Try to remove pip, poetry, and npm cache files more thoroughly

--- software/distro/setup/planktoscope-app-env/cleanup.sh
+++ software/distro/setup/planktoscope-app-env/cleanup.sh
@@ -7,12 +7,16 @@ sudo apt-get clean -y
 
 # Clean up any unnecessary pip, poetry, and npm files
 pip3 cache purge || true
+sudo rm -rf $HOME/.cache/pip
 POETRY_VENV=$HOME/.local/share/pypoetry/venv
 if [ -f $POETRY_VENV/bin/poetry ]; then
   BACKEND_CONTROLLER=$HOME/device-backend/control
   $POETRY_VENV/bin/poetry --no-interaction --directory $BACKEND_CONTROLLER cache clear _default_cache --all
   $POETRY_VENV/bin/poetry --no-interaction --directory $BACKEND_CONTROLLER cache clear piwheels --all
+  sudo rm -rf $HOME/.cache/pypoetry
 fi
+npm cache clean
+sudo npm cache clean
 
 # Remove history files
 rm -f $HOME/.python_history
