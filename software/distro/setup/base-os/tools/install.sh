PlanktoScope/PlanktoScope

commit c25c129ff0a9b4419d224fbd8996bf7ca2adeda7

Fix some errors which occur when re-running setup scripts

software/distro/setup/base-os/tools/install.sh
@@ -19,4 +19,5 @@ sudo apt-get install -y -o Dpkg::Progress-Fancy=0 \
 # Add a symlink at /var/lib/planktoscope/machine-name for backwards-compatibility with the Node-RED
 # frontend and Python backend, which are not yet updated to check /run/machine-name instead:
 sudo mkdir -p /var/lib/planktoscope
+sudo rm /var/lib/planktoscope/machine-name
 sudo ln -s /run/machine-name /var/lib/planktoscope/machine-name

software/distro/setup/base-os/forklift/install.sh
@@ -13,6 +13,7 @@ config_files_root=$(dirname $(realpath $BASH_SOURCE))
 # Prepare most of the necessary systemd units:
 sudo cp $config_files_root/usr/lib/systemd/system/* /usr/lib/systemd/system/
 sudo cp $config_files_root/usr/lib/systemd/system-preset/* /usr/lib/systemd/system-preset/
+sudo systemctl unmask forklift-apply.service # if it was masked, we must unmask it to apply preset
 sudo systemctl preset forklift-apply.service
 # Set up read-write filesystem overlays with forklift-managed layers for /etc and /usr
 # (see https://docs.kernel.org/filesystems/overlayfs.html):
@@ -71,7 +72,7 @@ echo "Preparing to load pre-downloaded container images..."
 sudo $tmp_bin/ctr --version
 # We load images with containerd instead of Docker so that we can do it without booting into a QEMU
 # VM (warning: Docker needs to be configured to use containerd for image storage!):
-if ! systemctl status containerd.service && ! sudo systemctl start containerd.service; then
+if ! systemctl --no-pager status containerd.service && ! sudo systemctl start containerd.service; then
   # We should only reach this if we're running setup in an unbooted container:
   echo "containerd.service couldn't be started; will try to start containerd directly..."
   sudo /usr/bin/containerd &

commit 6f3069da94405a79705ff90cc4d77af9581f72b9

Fix regression introduced by c25c129f

.github/workflows/build-os.yml
@@ -135,7 +135,6 @@ jobs:
               REPO="github.com/${{ github.repository }}"
             fi
             VERSION_QUERY="${{ env.ACTUAL_SHA }}"
-            cat /run/os-setup/.git/config
             # PR-triggered workflow runs have a merge commit which is detached from existing
             # branches and messes up pseudoversion determination, so instead we check out the
             # actual commit (and fetch all tags so we can determine a pseudoversion string):

software/distro/setup/base-os/tools/install.sh
@@ -19,5 +19,5 @@ sudo apt-get install -y -o Dpkg::Progress-Fancy=0 \
 # Add a symlink at /var/lib/planktoscope/machine-name for backwards-compatibility with the Node-RED
 # frontend and Python backend, which are not yet updated to check /run/machine-name instead:
 sudo mkdir -p /var/lib/planktoscope
-sudo rm /var/lib/planktoscope/machine-name
+sudo rm -f /var/lib/planktoscope/machine-name
 sudo ln -s /run/machine-name /var/lib/planktoscope/machine-name