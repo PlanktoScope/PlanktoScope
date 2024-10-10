dd596bf31162902871e7b23abf7fb0a86255415f
coulm <34215459+redhox@users.noreply.github.com>
2024-10-10 18:36:56 +0200

    fixing the typo on the systemd-timesyncd.service (#485)
    * fixing the typo on the systemd-timesyncd.service
    
    * change the order of disableing systemd timesynd and chrony instalation (when chony is install systemd-syncd is remove and cannot be disable)
    
    * try to install chrony upstream and so the service systemd-timesyncd.service while be remove
    
    * remove systemd-timesyncd.service management
    
    ---------
    
    Co-authored-by: coulm <morgan-coulm@gmail.com>

--- software/distro/setup/planktoscope-app-env/gps/install.sh
+++ software/distro/setup/planktoscope-app-env/gps/install.sh
@@ -23,8 +23,4 @@ fi
 file="/boot/config.txt"
 sudo bash -c "cat \"$config_files_root$file.snippet\" >> \"$file\""
 
-# Use chrony instead of systemd-timesyncd (but do we really actually want to do this??):
-if ! sudo systemctl disable systemd-timesyncd.service --now; then
-  # If we're in an unbooted container, the service isn't running - so we don't need to stop it now:
-  sudo systemctl disable systemd-timesyncd.servkce
-fi
+
