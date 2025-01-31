569bbc1df1d2a4fbfa3ad1f9f0ac2d5e79982ba8
Ethan Li <lietk12@gmail.com>
2025-01-31 11:53:44 -0800

    Try again to fix invocation of crane

--- software/distro/setup/base-os/forklift/precache-image.sh
+++ software/distro/setup/base-os/forklift/precache-image.sh
@@ -10,19 +10,19 @@ if [ -f "$precached_image" ]; then
   exit 0
 fi
 
-crane="crane"
+crane="$(command -v crane)"
 if ! mkdir -p "$(dirname "$precached_image")" &>/dev/null; then
   sudo mkdir -p "$(dirname "$precached_image")"
-  crane="sudo -E crane"
+  crane="sudo $crane"
 fi
 
 echo "Downloading $image to $precached_image..."
 # We pull images as Docker (legacy) tarballs so that they can be inspected with `dive` (which does
 # not support OCI tarballs) - see https://github.com/google/go-containerregistry/issues/621 for
 # details:
-if ! $crane --platform "$platform" pull --format legacy "$image" "$precached_image"; then
+if ! "$crane" --platform "$platform" pull --format legacy "$image" "$precached_image"; then
   echo "Encountered error, trying one more time to download $image..."
   rm -f "$precached_image" || sudo rm -f "$precached_image"
-  $crane --platform "$platform" pull --format legacy "$image" "$precached_image"
+  "$crane" --platform "$platform" pull --format legacy "$image" "$precached_image"
 fi
 echo "Finished downloading $image!"
