f8c6596bd69254e3858534ace237ec26eb70e1e7
Ethan Li <lietk12@gmail.com>
2025-01-31 11:57:45 -0800

    Fix autoformatter problem with invocation of crane

--- software/distro/setup/base-os/forklift/precache-image.sh
+++ software/distro/setup/base-os/forklift/precache-image.sh
@@ -20,9 +20,9 @@ echo "Downloading $image to $precached_image..."
 # We pull images as Docker (legacy) tarballs so that they can be inspected with `dive` (which does
 # not support OCI tarballs) - see https://github.com/google/go-containerregistry/issues/621 for
 # details:
-if ! "$crane" --platform "$platform" pull --format legacy "$image" "$precached_image"; then
+if ! $crane --platform "$platform" pull --format legacy "$image" "$precached_image"; then
   echo "Encountered error, trying one more time to download $image..."
   rm -f "$precached_image" || sudo rm -f "$precached_image"
-  "$crane" --platform "$platform" pull --format legacy "$image" "$precached_image"
+  $crane --platform "$platform" pull --format legacy "$image" "$precached_image"
 fi
 echo "Finished downloading $image!"
