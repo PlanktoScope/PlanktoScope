e317bc749492b498d978942de394f79b8b2dcaa0
Ethan Li <lietk12@gmail.com>
2025-02-03 16:53:07 -0800

    See what happens if we don't install hostapd anymore

--- software/distro/setup/base-os/networking/install.sh
+++ software/distro/setup/base-os/networking/install.sh
@@ -7,7 +7,7 @@ config_files_root=$(dirname "$(realpath "$BASH_SOURCE")")
 
 # Install dependencies
 sudo -E apt-get install -y -o Dpkg::Progress-Fancy=0 \
-  network-manager firewalld dnsmasq-base hostapd avahi-utils
+  network-manager firewalld dnsmasq-base avahi-utils
 sudo systemctl enable NetworkManager.service
 
 # Uninstall dhcpcd if we're on bullseye
@@ -20,7 +20,7 @@ fi
 # Set the wifi country
 # FIXME: instead have the user set the wifi country via a first-launch setup wizard, and do it
 # without using raspi-config. It should also be updated if the user changes the wifi country.
-# This should also update the hostapd config (maybe via a new template variable)
+# Or maybe we don't need to set the wifi country?
 if command -v raspi-config &>/dev/null; then
   sudo raspi-config nonint do_wifi_country US
 else
