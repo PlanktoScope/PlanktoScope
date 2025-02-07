c3486e95496c5889df782c3dfabd7016bac822ce
Ethan Li <lietk12@gmail.com>
2025-02-07 12:32:21 -0800

    Try to fix issues with Docker installation caused by previous commit

--- software/distro/setup/base-os/docker/install.sh
+++ software/distro/setup/base-os/docker/install.sh
@@ -11,7 +11,7 @@ fi
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 DISTRO_VERSION_CODENAME="$(. /etc/os-release && echo "$VERSION_CODENAME")"
 echo \
-  "deb [arch=" "$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] " \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg]" \
   "https://download.docker.com/linux/debian $DISTRO_VERSION_CODENAME stable" |
   sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo -E apt-get update -y -o Dpkg::Progress-Fancy=0 # get the list of packages from the docker repo
