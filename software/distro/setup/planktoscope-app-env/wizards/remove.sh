ad1094e9dc11c8cc5d229b3c099ec4f14278321d
Ethan Li <lietk12@gmail.com>
2025-02-21 10:03:32 -0800

    Try to fix bash syntax error

--- software/distro/setup/planktoscope-app-env/wizards/remove.sh
+++ software/distro/setup/planktoscope-app-env/wizards/remove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
 # Disable the first-boot graphical wizard
-if ! sudo -E apt-get purge -y -o Dpkg::Progress-Fancy=0 piwiz;
+if ! sudo -E apt-get purge -y -o Dpkg::Progress-Fancy=0 piwiz; then
   echo "piwiz is already not installed, so no need to remove it!"
 fi
