3f20aedc8a67ceefcbf2a9abd96aa0edb690c34f
Ethan Li <lietk12@gmail.com>
2025-02-21 14:33:18 -0800

    Run `apt autoremove` after purging piwiz

--- software/distro/setup/planktoscope-app-env/wizards/remove.sh
+++ software/distro/setup/planktoscope-app-env/wizards/remove.sh
@@ -3,4 +3,6 @@
 # Disable the first-boot graphical wizard
 if ! sudo -E apt-get purge -y -o Dpkg::Progress-Fancy=0 piwiz; then
   echo "piwiz is already not installed, so no need to remove it!"
+else
+  sudo -E apt-get autoremove -y -o Dpkg::Progress-Fancy=0
 fi
