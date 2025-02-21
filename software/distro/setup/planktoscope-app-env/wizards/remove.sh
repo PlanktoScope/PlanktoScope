7fb8219aa5b9342dd06cb74a61e97be302cd686b
Ethan Li <lietk12@gmail.com>
2025-02-21 09:46:12 -0800

    Try to remove piwiz from desktop images

---
+++ software/distro/setup/planktoscope-app-env/wizards/remove.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eux
+
+# Disable the first-boot graphical wizard
+if ! sudo -E apt-get purge -y -o Dpkg::Progress-Fancy=0 piwiz;
+  echo "piwiz is already not installed, so no need to remove it!"
+fi
