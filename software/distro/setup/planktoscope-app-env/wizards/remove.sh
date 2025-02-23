3392831c44f70e4b4c394a90e3c975d7d37054a2
Ethan Li <lietk12@gmail.com>
2025-02-23 08:23:18 -0800

    Troubleshoot removal of piwiz

--- software/distro/setup/planktoscope-app-env/wizards/remove.sh
+++ software/distro/setup/planktoscope-app-env/wizards/remove.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -eux
 
 # Disable the first-boot graphical wizard
-if ! sudo -E apt-get purge -y -o Dpkg::Progress-Fancy=0 piwiz; then
-  echo "piwiz is already not installed, so no need to remove it!"
-else
-  sudo -E apt-get autoremove -y -o Dpkg::Progress-Fancy=0
-fi
+# if ! sudo -E apt-get purge -y -o Dpkg::Progress-Fancy=0 piwiz; then
+#   echo "piwiz is already not installed, so no need to remove it!"
+# else
+#   sudo -E apt-get autoremove -y -o Dpkg::Progress-Fancy=0
+# fi
+rm -f /etc/xdg/autostart/piwiz.desktop
