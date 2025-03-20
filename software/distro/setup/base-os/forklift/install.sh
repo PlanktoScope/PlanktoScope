87385de4b8569f52c32b69ab9ec8345be0303cf9
Ethan Li <lietk12@gmail.com>
2025-03-19 17:35:32 -0700

    Update `forklift` invocation in OS build scripts

--- software/distro/setup/base-os/forklift/install.sh
+++ software/distro/setup/base-os/forklift/install.sh
@@ -30,7 +30,7 @@ fi
 # Clone & stage a local pallet
 pallet_path="$(cat "$config_files_root/forklift-pallet")"
 pallet_version="$(cat "$config_files_root/forklift-pallet-version")"
-forklift --stage-store /var/lib/forklift/stages plt switch --no-cache-img "$pallet_path@$pallet_version"
+forklift --stage-store /var/lib/forklift/stages plt switch --cache-img=false "$pallet_path@$pallet_version"
 forklift --stage-store /var/lib/forklift/stages stage add-bundle-name factory-reset next
 
 # Set up Forklift upgrade checks
@@ -79,16 +79,16 @@ echo "setup" | sudo tee /run/machine-name
 
 # Applying the staged pallet (i.e. making Docker instantiate all the containers) significantly
 # decreases first-boot time, by up to 30 sec for github.com/PlanktoScope/pallet-standard.
-if ! $FORKLIFT stage apply; then
+if ! "$FORKLIFT" stage apply; then
   echo "The staged pallet couldn't be applied; we'll try again now..."
   # Reset the "apply-failed" status of the staged pallet to apply:
-  $FORKLIFT stage set-next --no-cache-img next
-  if ! $FORKLIFT stage apply; then
+  "$FORKLIFT" stage set-next --cache-img=false next
+  if ! "$FORKLIFT" stage apply; then
     echo "Warning: the next staged pallet could not be successfully applied. We'll try again on the next boot, since the pallet might require some files which will only be created during the next boot."
     # Reset the "apply-failed" status of the staged pallet to apply:
-    $FORKLIFT stage set-next --no-cache-img next
+    "$FORKLIFT" stage set-next --cache-img=false next
     echo "Checking the plan for applying the staged pallet..."
-    $FORKLIFT stage plan
+    "$FORKLIFT" stage plan
   fi
 fi
 
