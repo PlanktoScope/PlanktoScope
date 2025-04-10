883b40daa7542c1e0cef6bd0ee85146525970438
Sonny Piers <sonny@fairscope.com>
2025-04-10 19:10:18 +0200

    f

--- software/distro/setup/planktoscope-app-env/PlanktoScope/install.sh
+++ software/distro/setup/planktoscope-app-env/PlanktoScope/install.sh
@@ -5,7 +5,5 @@
 # Determine the base path for copied files
 config_files_root="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-# Download monorepo
-backend_version="$(cat "$config_files_root/backend-version")"
-git clone "https://github.com/PlanktoScope/PlanktoScope.git" "$HOME/repo" --no-checkout --filter=blob:none
-git -C "$HOME/repo" checkout --quiet "$backend_version"
+cp -r "$repo_root" $HOME/PlanktoScope
+sudo chown -R pi:pi $HOME/PlanktoScope
