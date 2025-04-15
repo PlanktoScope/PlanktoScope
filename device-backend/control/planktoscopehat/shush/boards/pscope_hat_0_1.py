e45386e95ceb2595c39f81857bab55523facbff7
Sonny Piers <sonny@fairscope.com>
2025-04-15 18:22:59 +0200

    backend: Use gpiozero for steppers

--- device-backend/control/planktoscopehat/shush/boards/pscope_hat_0_1.py
+++ device-backend/control/planktoscopehat/shush/boards/pscope_hat_0_1.py
@@ -3,9 +3,9 @@ __author__ = "RBazile"
 # Define SPI Chip Select pin for each motor using RPi GPIO
 # Define enable pins to enable/disable motor
 
-m0_cs = 8
+m0_cs = 27
 m0_enable = 23
-m1_cs = 7
+m1_cs = 17
 m1_enable = 5
 error = 16
 stall = 20
