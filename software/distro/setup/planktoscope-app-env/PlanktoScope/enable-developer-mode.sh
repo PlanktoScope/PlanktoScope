7e2867d575fbeb3551d4cff8dd850c5003016559
Sonny Piers <sonny@fairscope.com>
2025-04-16 13:51:55 +0000

    f

--- software/distro/setup/planktoscope-app-env/PlanktoScope/enable-developer-mode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eux
-
-repo=$HOME/PlanktoScope
-cd $repo
-git init
-git remote add origin git@github.com:PlanktoScope/PlanktoScope.git
-git fetch origin --filter=blob:none
-git reset origin/HEAD
