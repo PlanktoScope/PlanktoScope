5746aab7ff76f7e99a190b03c2ad319d1e20d976
Sonny Piers <sonny@fairscope.com>
2025-04-17 07:50:04 +0000

    remove misleadng header

--- software/node-red-dashboard/settings.js
+++ software/node-red-dashboard/settings.js
@@ -1,24 +1,4 @@
-/**
- * This is the default settings file provided by Node-RED.
- *
- * It can contain any valid JavaScript code that will get run when Node-RED
- * is started.
- *
- * Lines that start with // are commented out.
- * Each entry should be separated from the entries above and below by a comma ','
- *
- * For more information about individual settings, refer to the documentation:
- *    https://nodered.org/docs/user-guide/runtime/configuration
- *
- * The settings are split into the following sections:
- *  - Flow File and User Directory Settings
- *  - Security
- *  - Server Settings
- *  - Runtime Settings
- *  - Editor Settings
- *  - Node Settings
- *
- **/
+// https://nodered.org/docs/user-guide/runtime/configuration
 
 module.exports = {
 
