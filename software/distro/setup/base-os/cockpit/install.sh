51f1031ad81dcee1e5feb6fe0624e71e8ac82e37
Ethan Li <lietk12@gmail.com>
2025-05-07 11:00:26 -0700

    Troubleshoot update-initramfs's configuration

--- software/distro/setup/base-os/cockpit/install.sh
+++ software/distro/setup/base-os/cockpit/install.sh
@@ -2,5 +2,11 @@
 # Cockpit enables system administration from a web browser.
 
 # Install cockpit
+# Note: we need to adjust update-initramfs's behavior to make apt-get finish successfully; see
+# https://github.com/PlanktoScope/PlanktoScope/pull/596 and
+# https://github.com/RPi-Distro/repo/issues/382 for details.
+grep -r MODULES /etc/initramfs-tools
+sed -i 's~MODULES=dep~MODULES=most~' /etc/initramfs-tools/initramfs.conf
 sudo -E apt-get install -y --no-install-recommends -o Dpkg::Progress-Fancy=0 \
-  cockpit cockpit-networkmanager cockpit-storaged
+	cockpit cockpit-networkmanager cockpit-storaged
+sed -i 's~MODULES=most~MODULES=dep~' /etc/initramfs-tools/initramfs.conf
