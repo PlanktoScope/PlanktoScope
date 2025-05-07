8e56be6d6d87d8686c4b0d3a8d7c050a9e946e46
Ethan Li <lietk12@gmail.com>
2025-05-07 11:13:15 -0700

    Try to fix permissions issues with previous commit

--- software/distro/setup/base-os/cockpit/install.sh
+++ software/distro/setup/base-os/cockpit/install.sh
@@ -5,8 +5,13 @@
 # Note: we need to adjust update-initramfs's behavior to make apt-get finish successfully; see
 # https://github.com/PlanktoScope/PlanktoScope/pull/596 and
 # https://github.com/RPi-Distro/repo/issues/382 for details.
-grep -r MODULES /etc/initramfs-tools
-sed -i 's~MODULES=dep~MODULES=most~' /etc/initramfs-tools/initramfs.conf
+adjust_initramfs_scope=false
+if grep -q 'MODULES=dep' /etc/initramfs-tools/initramfs.conf; then
+	adjust_initramfs_scope=true
+	sudo sed -i 's~MODULES=dep~MODULES=most~' /etc/initramfs-tools/initramfs.conf
+fi
 sudo -E apt-get install -y --no-install-recommends -o Dpkg::Progress-Fancy=0 \
 	cockpit cockpit-networkmanager cockpit-storaged
-sed -i 's~MODULES=most~MODULES=dep~' /etc/initramfs-tools/initramfs.conf
+if [ "$adjust_initramfs_scope" = true ]; then
+	sudo sed -i 's~MODULES=most~MODULES=dep~' /etc/initramfs-tools/initramfs.conf
+fi
