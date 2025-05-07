7583c87ca49898203c5e12962a09a4d0aaf430e7
Ethan Li <lietk12@gmail.com>
2025-05-07 11:37:26 -0700

    Update another bootloader `config.txt` path

--- software/distro/setup/planktoscope-app-env/gps/install.sh
+++ software/distro/setup/planktoscope-app-env/gps/install.sh
@@ -7,7 +7,7 @@ config_files_root=$(dirname "$(realpath "$BASH_SOURCE")")
 
 # Install dependencies
 sudo -E apt-get install -y -o Dpkg::Progress-Fancy=0 \
-  gpsd pps-tools chrony
+	gpsd pps-tools chrony
 
 # The following command enables the serial port but disables the login shell over the serial port.
 # We use this because the serial port is reserved for a GPS device.
@@ -15,5 +15,5 @@ sudo -E apt-get install -y -o Dpkg::Progress-Fancy=0 \
 sudo raspi-config nonint do_serial_cons 1
 
 # Enable automatic time update from GPSD
-file="/boot/config.txt"
+file="/boot/firmware/config.txt"
 sudo bash -c "cat \"$config_files_root$file.snippet\" >> \"$file\""
