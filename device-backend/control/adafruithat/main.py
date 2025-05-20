PlanktoScope/PlanktoScope

commit 33a97d50ab08aea94090595d63fb40b3beae82a0

software: Gitify node-red-dashboard (#573)
Configure node-red to use the git repository as `userDir` so that
changes to flows are visible in git.

Here is the output of `git status` after making changed in the node-red
editor:

![Screenshot From 2025-04-16
13-36-41](https://github.com/user-attachments/assets/a9f6b68f-1a04-44ff-84ce-0aa66bdc055d)

`settings.js` file was copied from
https://github.com/PlanktoScope/pallet-standard/blob/8fd23fded73b71d391d47a14ca79684cfcb6aeb7/packages/core/apps/planktoscope/node-red-dashboard/overlays/etc/nodered/settings.js

Previously the setting file was setup using forklift
https://github.com/PlanktoScope/pallet-standard/blob/8fd23fded73b71d391d47a14ca79684cfcb6aeb7/packages/core/apps/planktoscope/node-red-dashboard/overlays/etc/systemd/system/nodered.service.d/20-override.conf

This is "bypassing" forklift as I'm not familiar with it yet, it is
unclear if it will remain and in any case [dashboard
2](https://github.com/PlanktoScope/dashboard) will likely deprecate
this.


![image](https://github.com/user-attachments/assets/8437615a-a2b4-4a15-9905-ad74c086be6a)

software/distro/setup/planktoscope-app-env/node-red-frontend/install.sh
@@ -3,7 +3,6 @@
 
 # Determine the base path for copied files
 config_files_root=$(dirname "$(realpath "$BASH_SOURCE")")
-distro_setup_files_root=$(dirname "$(dirname "$config_files_root")")
 
 # Get command-line args
 hardware_type="$1" # should be either adafruithat, planktoscopehat, fairscope-latest, or segmenter-only
@@ -36,7 +35,6 @@ sudo ln -s /run/machine-name /var/lib/planktoscope/machine-name
 # default, we do `pip3 install` as the pi user. This makes the smbus2 module available to Node-RED.
 # FIXME: get rid of the Node-RED nodes depending on smbus! That functionality should be moved into
 # the Python backend.
-# Note: for bookworm we need to install the apt package; for bullseye there is no apt package
 if ! sudo apt-get install -y python3-smbus2; then
   sudo apt-get install -y python3-pip
   pip3 install smbus2==0.4.3
@@ -47,15 +45,13 @@ fi
 curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered |
   bash -s - --confirm-install --confirm-pi --no-init
 
-# Select the enabled dashboard
-mkdir -p "$HOME"/.node-red
-cp "$HOME/PlanktoScope/software/node-red-dashboard/flows/$hardware_type.json" \
-  "$HOME"/.node-red/flows.json
-mkdir -p "$HOME"/PlanktoScope
 cp "$HOME/PlanktoScope/software/node-red-dashboard/default-configs/$default_config.config.json" \
   "$HOME"/PlanktoScope/config.json
 
-# Install dependencies in a way that makes them available to Node-RED
-cp "$HOME"/PlanktoScope/software/node-red-dashboard/package.json "$HOME"/.node-red/
-cp "$HOME"/PlanktoScope/software/node-red-dashboard/package-lock.json "$HOME"/.node-red/
-npm --prefix "$HOME"/.node-red update
+# Configure node-red
+npm --prefix "$HOME"/PlanktoScope/software/node-red-dashboard install
+sudo mkdir -p /etc/systemd/system/nodered.service.d
+sudo cp $config_files_root/30-override.conf /etc/systemd/system/nodered.service.d/30-override.conf
+
+# Install dependencies to make them available to Node-RED
+npm --prefix "$HOME"/PlanktoScope/software/node-red-dashboard/$hardware_type install

software/distro/setup/planktoscope-app-env/PlanktoScope/enable-developer-mode
@@ -11,5 +11,3 @@ git fetch origin --filter=blob:none
 # "Resets the index but not the working tree (i.e., the changed files are preserved but not marked for commit) and reports what has not been updated."
 # https://git-scm.com/docs/git-reset#Documentation/git-reset.txt---mixed
 git reset --mixed $commit
-
-mkdir $HOME/.ssh

software/distro/setup/planktoscope-app-env/node-red-frontend/30-override.conf
@@ -1,0 +1,11 @@
+[Unit]
+# Start before mosquitto (provided via Forklift), to (hopefully) ensure that the Node-RED dashboard
+# is ready to catch all MQTT messages, such as messages sent by the Python backend right when it
+# starts (FIXME: make that unnecessary by allowing Node-RED to query the Python backend for any
+# information it needs later):
+Wants=forklift-apply.service
+Before=forklift-apply.service
+
+[Service]
+ExecStart=
+ExecStart=/usr/bin/env node-red-pi $NODE_OPTIONS --settings /home/pi/PlanktoScope/software/node-red-dashboard/settings.js $NODE_RED_OPTIONS

software/node-red-dashboard/.gitignore
@@ -1,0 +1,2 @@
+*.backup
+.config.*.json

commit 5bf4c89d69ebda96d22e016b2280f6f18178f1ef

Select hardware based on `config.json` file (#602)
As part of #182, this PR enables the user to switch between the
adafruithat vs. planktoscopehat program variants by editing the SD
card's `~/PlanktoScope/config.json` file and then rebooting (or
restarting `nodered.service` and then restarting
`planktoscope-org.device-backend.controller.service` after Node-RED
becomes ready to receive MQTT messages, without a full system reboot). A
minor caveat here is that GPS functionality is not available on
planktoscopehat-based images running the adafruithat program variants
(because of #591); but in the [2025-05-14 software
meeting](https://docs.google.com/document/d/1AN1jaIdfKChKjMnzSd3ecjRTZGOnoglBfV9LCpevMgk/edit?tab=t.geukyqpm9ntj#heading=h.ziivhaa9au6q)
I plan to propose deprecating GPS functionality from the adafruithat
variants too.

Specifically, this PR:

- unifies the systemd services for the Python device controller into a
single service, and this PR provides a single top-level entrypoint
script which selects the adafruithat vs. planktoscopehat `main.py`
script based on the ~configuration in
`/usr/share/planktoscope/installer-config.yml`, in the same way that
#573 selects adafruithat vs. planktoscopehat for the Node-RED dashboard~
`~/PlanktoScope/config.json` file's `acq_instrument` field (whose values
are like `PlanktoScope v2.6`). See
https://github.com/PlanktoScope/PlanktoScope/pull/583#issuecomment-2828380359
and the [2025-05-07 software
meeting](https://docs.google.com/document/d/1AN1jaIdfKChKjMnzSd3ecjRTZGOnoglBfV9LCpevMgk/edit?tab=t.geukyqpm9ntj#heading=h.81oklfxjwmel)
for additional context on this step towards a piecemeal incremental
unification of the adafruithat vs. planktoscopehat variants of the
Python device controller.
- updates the Node-RED dashboard's `settings.js` file to choose between
the adafruithat vs. planktoscopehat variants of the dashboard based on
the `~/PlanktoScope/config.json` file's `acq_instrument` field.
- fixes a regression in the `fairscope-latest` image caused by #573,

device-backend/control/adafruithat/main.py
@@ -23,37 +23,8 @@ import os
 
 from loguru import logger  # for logging with multiprocessing
 
-import planktoscope.mqtt
-import planktoscope.stepper
-import planktoscope.light  # Fan HAT LEDs
-import planktoscope.identity
-import planktoscope.display  # Fan HAT OLED screen
-from planktoscope.imager import mqtt as imager
-
-# enqueue=True is necessary so we can log across modules
-# rotation happens everyday at 01:00 if not restarted
-logs_path = "/home/pi/device-backend-logs/control"
-if not os.path.exists(logs_path):
-    os.makedirs(logs_path)
-logger.add(
-    # sys.stdout,
-    "/home/pi/device-backend-logs/control/{time}.log",
-    rotation="5 MB",
-    retention="1 week",
-    compression=".tar.gz",
-    enqueue=True,
-    level="DEBUG",
-)
-
-# The available level for the logger are as follows:
-# Level name 	Severity 	Logger method
-# TRACE 	    5 	        logger.trace()
-# DEBUG 	    10 	        logger.debug()
-# INFO 	        20 	        logger.info()
-# SUCCESS 	    25 	        logger.success()
-# WARNING 	    30      	logger.warning()
-# ERROR 	    40       	logger.error()
-# CRITICAL 	    50      	logger.critical()
+from adafruithat.planktoscope import stepper, light, identity, display
+from adafruithat.planktoscope.imager import mqtt as imager
 
 logger.info("Starting the PlanktoScope hardware controller!")
 
@@ -67,7 +38,7 @@ def handler_stop_signals(signum, _):
     run = False
 
 
-if __name__ == "__main__":
+def main():
     logger.info("Welcome!")
     logger.info(
         "Initialising signals handling and sanitizing the directories (step 1/4)"
@@ -97,9 +68,7 @@ if __name__ == "__main__":
         # create the path!
         os.makedirs(img_path)
 
-    logger.info(
-        f"This PlanktoScope's machine name is {planktoscope.identity.load_machine_name()}"
-    )
+    logger.info(f"This PlanktoScope's machine name is {identity.load_machine_name()}")
 
     # Prepare the event for a graceful shutdown
     shutdown_event = multiprocessing.Event()
@@ -107,7 +76,7 @@ if __name__ == "__main__":
 
     # Starts the stepper process for actuators
     logger.info("Starting the stepper control process (step 2/4)")
-    stepper_thread = planktoscope.stepper.StepperProcess(shutdown_event)
+    stepper_thread = stepper.StepperProcess(shutdown_event)
     stepper_thread.start()
 
     # Starts the imager control process
@@ -121,10 +90,10 @@ if __name__ == "__main__":
         imager_thread.start()
 
     logger.info("Starting the display module (step 4/4)")
-    display = planktoscope.display.Display()
+    display = display.Display()
 
     logger.success("Looks like the controller is set up and running, have fun!")
-    planktoscope.light.ready()
+    light.ready()
 
     while run:
         # TODO look into ways of restarting the dead threads

device-backend/control/adafruithat/planktoscope/camera/mqtt.py
@@ -8,8 +8,8 @@ import typing
 
 import loguru
 
-from planktoscope import mqtt as messaging
-from planktoscope.camera import hardware, mjpeg
+from .. import mqtt as messaging
+from . import hardware, mjpeg
 
 loguru.logger.info("planktoscope.camera is loaded")
 

device-backend/control/adafruithat/planktoscope/display.py
@@ -18,18 +18,15 @@
 # Logger library compatible with multiprocessing
 from loguru import logger
 
-import datetime
-import os
-
 import Adafruit_SSD1306
 
 import PIL.Image
 import PIL.ImageDraw
 import PIL.ImageFont
 
-logger.info("planktoscope.display is loading")
+from . import identity
 
-import planktoscope.identity
+logger.info("planktoscope.display is loading")
 
 
 class Display(object):
@@ -54,7 +51,7 @@ class Display(object):
     def display_machine_name(self):
         if self.display_available:
             self.__clear()
-            machineName = planktoscope.identity.load_machine_name()
+            machineName = identity.load_machine_name()
             self.display_text(machineName.replace(" ", "\n"))
 
     def display_text(self, message):

device-backend/control/adafruithat/planktoscope/imager/mqtt.py
@@ -10,9 +10,9 @@ import typing
 
 import loguru
 
-from planktoscope import identity, integrity, mqtt
-from planktoscope.camera import mqtt as camera
-from planktoscope.imager import stopflow
+from .. import identity, integrity, mqtt
+from ..camera import mqtt as camera
+from . import stopflow
 
 loguru.logger.info("planktoscope.imager is loaded")
 

device-backend/control/adafruithat/planktoscope/stepper.py
@@ -21,10 +21,10 @@ import adafruit_motorkit
 import time
 import json
 import os
-import planktoscope.mqtt
-import planktoscope.light
 import multiprocessing
 import RPi.GPIO
+
+from . import mqtt, light
 
 # Logger library compatible with multiprocessing
 from loguru import logger
@@ -203,11 +203,11 @@ class StepperProcess(multiprocessing.Process):
                 "status/pump", '{"status":"Interrupted"}'
             )
 
-            planktoscope.light.ready()
+            light.ready()
 
         elif last_message["action"] == "move":
             logger.debug("We have received a move pump command")
-            planktoscope.light.pumping()
+            light.pumping()
 
             if (
                 "direction" not in last_message
@@ -256,11 +256,11 @@ class StepperProcess(multiprocessing.Process):
                 "status/focus", '{"status":"Interrupted"}'
             )
 
-            planktoscope.light.ready()
+            light.ready()
 
         elif last_message["action"] == "move":
             logger.debug("We have received a move focus command")
-            planktoscope.light.focusing()
+            light.focusing()
 
             if "direction" not in last_message or "distance" not in last_message:
                 logger.error(
@@ -422,7 +422,7 @@ class StepperProcess(multiprocessing.Process):
         # it doesn't see changes and calls made by self.actuator_client because this one
         # only exist in the master process
         # see https://stackoverflow.com/questions/17172878/using-pythons-multiprocessing-process-class
-        self.actuator_client = planktoscope.mqtt.MQTT_Client(
+        self.actuator_client = mqtt.MQTT_Client(
             topic="actuator/#", name="actuator_client"
         )
         # Publish the status "Ready" to via MQTT to Node-RED
@@ -437,14 +437,14 @@ class StepperProcess(multiprocessing.Process):
                 self.treat_command()
             if self.pump_stepper.move():
                 delay = 0.0001
-                planktoscope.light.ready()
+                light.ready()
                 self.actuator_client.client.publish(
                     "status/pump",
                     '{"status":"Done"}',
                 )
             if self.focus_stepper.move():
                 delay = 0.0001
-                planktoscope.light.ready()
+                light.ready()
                 self.actuator_client.client.publish(
                     "status/focus",
                     '{"status":"Done"}',

device-backend/control/main.py
@@ -1,0 +1,80 @@
+import json
+from os import makedirs, path
+
+from loguru import logger
+
+# enqueue=True is necessary so we can log across modules
+# rotation happens everyday at 01:00 if not restarted
+logs_path = "/home/pi/device-backend-logs/control"
+if not path.exists(logs_path):
+    makedirs(logs_path)
+logger.add(
+    # sys.stdout,
+    "/home/pi/device-backend-logs/control/{time}.log",
+    rotation="5 MB",
+    retention="1 week",
+    compression=".tar.gz",
+    enqueue=True,
+    level="DEBUG",
+)
+
+# The available level for the logger are as follows:
+# Level name 	Severity 	Logger method
+# TRACE 	    5 	        logger.trace()
+# DEBUG 	    10 	        logger.debug()
+# INFO 	        20 	        logger.info()
+# SUCCESS 	    25 	        logger.success()
+# WARNING 	    30      	logger.warning()
+# ERROR 	    40       	logger.error()
+# CRITICAL 	    50      	logger.critical()
+
+# This is a special case for legacy hardware; new hardware designs should all be part of the
+# planktoscopehat codebase:
+CONFIG_PATH = "/home/pi/PlanktoScope/config.json"
+
+
+def load_variant_setting(config_path: str = CONFIG_PATH):
+    config = {}
+    try:
+        with open(config_path, "r") as file:
+            try:
+                config = json.load(file)
+            except Exception:
+                logger.exception(f"Couldn't parse {config_path} as JSON file")
+                return None
+    except Exception:
+        logger.exception(f"Couldn't open {config_path}")
+        return None
+
+    if "acq_instrument" not in config:
+        logger.error(f"{config_path} lacks a 'acq_instrument' field")
+        return None
+
+    if config["acq_instrument"] == "PlanktoScope v2.1":
+        return "adafruithat"
+    return "planktoscopehat"
+
+
+def main():
+    logger.info("Determining configured hardware variant...")
+    variant = load_variant_setting()
+    if variant is None:
+        variant = "planktoscopehat"
+        logger.warning(
+            f"Couldn't load hardware variant setting from config, defaulting to {variant}"
+        )
+    logger.info(f"Hardware variant: {variant}")
+
+    if variant == "adafruithat":
+        from adafruithat import main as platform  # noqa: IMR200
+    else:
+        from planktoscopehat import main as platform  # noqa: IMR200
+
+    platform.main()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        logger.exception("Unhandled exception")

device-backend/control/planktoscopehat/main.py
@@ -7,37 +7,8 @@ import json
 
 from loguru import logger
 
-import planktoscope.mqtt
-import planktoscope.pump
-import planktoscope.focus
-import planktoscope.light
-import planktoscope.identity
-from planktoscope.imager import mqtt as imager
-
-# enqueue=True is necessary so we can log across modules
-# rotation happens everyday at 01:00 if not restarted
-logs_path = "/home/pi/device-backend-logs/control"
-if not os.path.exists(logs_path):
-    os.makedirs(logs_path)
-logger.add(
-    # sys.stdout,
-    "/home/pi/device-backend-logs/control/{time}.log",
-    rotation="5 MB",
-    retention="1 week",
-    compression=".tar.gz",
-    enqueue=True,
-    level="DEBUG",
-)
-
-# The available level for the logger are as follows:
-# Level name 	Severity 	Logger method
-# TRACE 	    5 	        logger.trace()
-# DEBUG 	    10 	        logger.debug()
-# INFO 	        20 	        logger.info()
-# SUCCESS 	    25 	        logger.success()
-# WARNING 	    30      	logger.warning()
-# ERROR 	    40       	logger.error()
-# CRITICAL 	    50      	logger.critical()
+from planktoscopehat.planktoscope import pump, focus, light, identity
+from planktoscopehat.planktoscope.imager import mqtt as imager
 
 logger.info("Starting the PlanktoScope python script!")
 
@@ -51,7 +22,7 @@ def handler_stop_signals(signum, frame):
     run = False
 
 
-if __name__ == "__main__":
+def main():
     logger.info("Welcome!")
     logger.info(
         "Initialising configuration, signals handling and sanitizing the directories (step 1/5)"
@@ -81,9 +52,7 @@ if __name__ == "__main__":
         # create the path!
         os.makedirs(img_path)
 
-    logger.info(
-        f"This PlanktoScope's machine name is {planktoscope.identity.load_machine_name()}"
-    )
+    logger.info(f"This PlanktoScope's machine name is {identity.load_machine_name()}")
 
     try:
         with open("/home/pi/PlanktoScope/hardware.json", "r") as config_file:
@@ -100,12 +69,12 @@ if __name__ == "__main__":
 
     # Starts the pump process
     logger.info("Starting the pump control process (step 2/5)")
-    pump_thread = planktoscope.pump.PumpProcess(shutdown_event, configuration)
+    pump_thread = pump.PumpProcess(shutdown_event, configuration)
     pump_thread.start()
 
     # Starts the focus process
     logger.info("Starting the focus control process (step 3/5)")
-    focus_thread = planktoscope.focus.FocusProcess(shutdown_event, configuration)
+    focus_thread = focus.FocusProcess(shutdown_event, configuration)
     focus_thread.start()
 
     # TODO try to isolate the imager thread (or another thread)
@@ -122,7 +91,7 @@ if __name__ == "__main__":
     # Starts the light process
     logger.info("Starting the light control process (step 5/5)")
     try:
-        light_thread = planktoscope.light.LightProcess(shutdown_event, configuration)
+        light_thread = light.LightProcess(shutdown_event, configuration)
     except Exception:
         logger.error("The light control process could not be started")
         light_thread = None

device-backend/control/planktoscopehat/planktoscope/camera/mqtt.py
@@ -7,8 +7,8 @@ import typing
 
 import loguru
 
-from planktoscope import mqtt as messaging
-from planktoscope.camera import hardware, mjpeg
+from .. import mqtt as messaging
+from . import hardware, mjpeg
 
 loguru.logger.info("planktoscope.camera is loaded")
 

device-backend/control/planktoscopehat/planktoscope/focus.py
@@ -4,9 +4,8 @@ import time
 
 from loguru import logger
 
-from planktoscope.stepper import stepper
-
 from . import mqtt
+from .stepper import stepper
 
 logger.info("planktoscope.focus is loaded")
 

device-backend/control/planktoscopehat/planktoscope/imager/mqtt.py
@@ -10,9 +10,9 @@ import typing
 
 import loguru
 
-from planktoscope import identity, integrity, mqtt
-from planktoscope.camera import mqtt as camera
-from planktoscope.imager import stopflow
+from .. import identity, integrity, mqtt
+from ..camera import mqtt as camera
+from . import stopflow
 
 loguru.logger.info("planktoscope.imager is loaded")
 

device-backend/control/planktoscopehat/planktoscope/module.py
@@ -10,7 +10,7 @@ import os, time
 import multiprocessing
 
 # Basic planktoscope communication libraries
-import planktoscope.mqtt
+from . import mqtt
 
 logger.info("planktoscope.module is loaded")
 
@@ -69,7 +69,7 @@ class ModuleProcess(multiprocessing.Process):
         )
 
         # MQTT Service connection
-        self.module_client = planktoscope.mqtt.MQTT_Client(
+        self.module_client = mqtt.MQTT_Client(
             topic="module/#", name="module_client"
         )
 

device-backend/control/planktoscopehat/planktoscope/pump.py
@@ -4,9 +4,8 @@ import time
 
 from loguru import logger
 
-from planktoscope.stepper import stepper
-
 from . import mqtt
+from .stepper import stepper
 
 logger.info("planktoscope.pump is loaded")
 

software/distro/setup/ci-record-version.sh
@@ -5,7 +5,7 @@
 # REPO (the repo used for setup, e.g. github.com/PlanktoScope/PlanktoScope)
 # VERSION_QUERY (the version query, e.g. a commit hash)
 # QUERY_TYPE (eitiher branch, tag, or hash)
-# HARDWARE (either none, segmenter-only, adafruithat, or planktoscopehat)
+# HARDWARE (either none, adafruithat, or planktoscopehat)
 # VERSION_QUERY_DIR (the filesystem path of the git repo used for getting version info)
 
 # Utilities for user interaction

software/distro/setup/cleanup.sh
@@ -7,7 +7,7 @@ setup_scripts_root=$(dirname $(realpath $BASH_SOURCE))
 
 # Get command-line args
 
-hardware_type="$1" # should be either none, adafruithat, planktoscopehat, or segmenter-only
+hardware_type="$1" # should be either none, adafruithat, or planktoscopehat
 
 # Set up pretty error printing
 

software/distro/setup/planktoscope-app-env/node-red-frontend/install.sh
@@ -5,20 +5,12 @@
 config_files_root=$(dirname "$(realpath "$BASH_SOURCE")")
 
 # Get command-line args
-hardware_type="$1" # should be either adafruithat, planktoscopehat, fairscope-latest, or segmenter-only
+hardware_type="$1" # should be either adafruithat, planktoscopehat, or fairscope-latest
 default_config="$hardware_type-latest"
-case "$hardware_type" in
-"fairscope-latest")
+if [ "$hardware_type" = "fairscope-latest" ]; then
   hardware_type="planktoscopehat"
   default_config="fairscope-latest"
-  ;;
-"segmenter-only")
-  # FIXME: instead set up the segmenter-only version of the Node-RED dashboard!
-  echo "Warning: setting up adafruithat version of Node-RED dashboard for hardware type: $hardware_type"
-  hardware_type=adafruithat
-  default_config="adafruithat-latest"
-  ;;
-esac
+fi
 
 # Install dependencies
 # smbus is needed by some python3 nodes in the Node-RED dashboard for the Adafruit HAT.
@@ -50,5 +42,5 @@ cp "$HOME/PlanktoScope/software/node-red-dashboard/default-configs/$default_conf
   "$HOME"/PlanktoScope/config.json
 
 # Configure node-red
-npm --prefix "$HOME"/PlanktoScope/software/node-red-dashboard install
-npm --prefix "$HOME"/PlanktoScope/software/node-red-dashboard/$hardware_type install
+npm --prefix "$HOME/PlanktoScope/software/node-red-dashboard/adafruithat" install
+npm --prefix "$HOME/PlanktoScope/software/node-red-dashboard/planktoscopehat" install

software/distro/setup/planktoscope-app-env/python-hardware-controller/etc/systemd/system/planktoscope-org.device-backend.controller-planktoscopehat.service
@@ -1,18 +1,0 @@
-[Unit]
-Description=Run the PlanktoScope hardware controller for the PlanktoScope HAT
-# Wait for Forklift to bring up mosquitto, in case the controller needs to start after mosquitto
-Wants=forklift-apply.service
-After=forklift-apply.service
-# Start after Node-RED so that Node-RED can catch all MQTT messages emitted at startup
-After=nodered.service
-
-[Service]
-Type=simple
-Environment=HOME=/home/pi
-WorkingDirectory=/home/pi/PlanktoScope/device-backend/control
-ExecStart=/home/pi/.local/bin/poetry run python -u planktoscopehat/main.py
-User=pi
-Group=pi
-
-[Install]
-WantedBy=multi-user.target

software/distro/setup/planktoscope-app-env/python-hardware-controller/etc/systemd/system/planktoscope-org.device-backend.controller.service
@@ -1,5 +1,5 @@
 [Unit]
-Description=Run the PlanktoScope hardware controller for the Adafruit HAT
+Description=Run the PlanktoScope hardware controller
 # Wait for Forklift to bring up mosquitto, in case the controller needs to start after mosquitto
 Wants=forklift-apply.service
 After=forklift-apply.service
@@ -10,7 +10,7 @@ After=nodered.service
 Type=simple
 Environment=HOME=/home/pi
 WorkingDirectory=/home/pi/PlanktoScope/device-backend/control
-ExecStart=/home/pi/.local/bin/poetry run python -u adafruithat/main.py
+ExecStart=/home/pi/.local/bin/poetry run python -u main.py
 User=pi
 Group=pi
 

software/distro/setup/planktoscope-app-env/python-hardware-controller/install.sh
@@ -34,14 +34,11 @@ sudo -E apt-get install -y --no-install-recommends -o Dpkg::Progress-Fancy=0 \
   i2c-tools libopenjp2-7 python3-picamera2
 poetry --directory "$HOME/PlanktoScope/device-backend/control" install \
   --no-root --compile
-file="/etc/systemd/system/planktoscope-org.device-backend.controller-adafruithat.service"
+file="/etc/systemd/system/planktoscope-org.device-backend.controller.service"
 sudo cp "$config_files_root$file" "$file"
-# or for the PlanktoScope HAT
-file="/etc/systemd/system/planktoscope-org.device-backend.controller-planktoscopehat.service"
-sudo cp "$config_files_root$file" "$file"
+sudo systemctl enable "planktoscope-org.device-backend.controller.service"
 
 # Select the enabled hardware controller
 mkdir -p "$HOME/PlanktoScope"
-sudo systemctl enable "planktoscope-org.device-backend.controller-$hardware_type.service"
 cp "$HOME/PlanktoScope/device-backend/default-configs/$default_config.hardware.json" \
   "$HOME/PlanktoScope/hardware.json"

software/distro/setup/planktoscope-app-env/setup.sh
@@ -11,7 +11,7 @@ build_scripts_root=$(dirname "$(realpath "$BASH_SOURCE")")
 
 # Get command-line args
 
-hardware_type="$1" # should be either adafruithat, planktoscopehat, fairscope-latest, or segmenter-only
+hardware_type="$1" # should be either adafruithat, planktoscopehat, fairscope-latest
 
 # Set up pretty error printing
 
@@ -51,11 +51,6 @@ if "$build_scripts_root/node-red-frontend/install.sh" "$hardware_type"; then
   report_finished "$description"
 else
   panic "$description"
-fi
-
-if [ "$hardware_type" = "segmenter-only" ]; then
-  echo "Warning: skipping PlanktoScope hardware-specific setup because hardware type was specified as: $hardware_type"
-  exit 0
 fi
 
 description="set up Python hardware controller"

software/distro/setup/setup.sh
@@ -11,7 +11,7 @@ setup_scripts_root=$(dirname "$(realpath "$BASH_SOURCE")")
 
 # Get command-line args
 
-hardware_type="$1" # should be either none, adafruithat, planktoscopehat, fairscope-latest, or segmenter-only
+hardware_type="$1" # should be either none, adafruithat, planktoscopehat, or fairscope-latest
 
 # Set up pretty error printing
 

software/node-red-dashboard/settings.js
@@ -1,13 +1,48 @@
 // https://nodered.org/docs/user-guide/runtime/configuration
 
-const yaml = require("js-yaml");
 const fs = require("fs");
 const path = require("path");
 
-const doc = yaml.load(
-	fs.readFileSync("/usr/share/planktoscope/installer-config.yml", "utf8"),
-);
-const userDir = path.join(__dirname, doc.hardware);
+// This is a special case for legacy hardware; new hardware designs should all be part of the
+// planktoscopehat codebase:
+const CONFIG_PATH = "/home/pi/PlanktoScope/config.json";
+function load_variant_setting(config_path = CONFIG_PATH) {
+	let config = {};
+	try {
+		const file = fs.readFileSync(config_path, "utf8");
+		try {
+			config = JSON.parse(file);
+		} catch (e) {
+			console.error(`Couldn't parse ${config_path} as JSON file`);
+			return undefined;
+		}
+	} catch (e) {
+		console.error(`Couldn't open ${config_path}`);
+		return undefined;
+	}
+
+	if (config.acq_instrument === undefined) {
+		console.error(`${config_path} lacks a 'acq_instrument' field`);
+		return undefined;
+	}
+
+	if (config.acq_instrument === "PlanktoScope v2.1") {
+		return "adafruithat";
+	}
+	return "planktoscopehat";
+}
+
+
+console.log("Determining configured hardware variant...");
+let variant = load_variant_setting();
+if (variant === undefined) {
+	variant = "planktoscopehat";
+	console.warn(
+		`Couldn't load hardware variant setting from config, defaulting to ${variant}`,
+	);
+}
+console.log(`Hardware variant: ${variant}`);
+const userDir = path.join(__dirname, variant);
 
 module.exports = {
 	/*******************************************************************************

device-backend/control/adafruithat/planktoscope/camera/test_preview.py
@@ -8,7 +8,7 @@ import loguru
 import picamera2  # type: ignore
 from picamera2 import encoders, outputs
 
-from planktoscope.camera import hardware, mjpeg
+from . import hardware, mjpeg
 
 
 def main() -> None:

device-backend/control/planktoscopehat/planktoscope/camera/test_preview.py
@@ -8,7 +8,7 @@ import loguru
 import picamera2  # type: ignore
 from picamera2 import encoders, outputs
 
-from planktoscope.camera import hardware, mjpeg
+from . import hardware, mjpeg
 
 
 def main() -> None: