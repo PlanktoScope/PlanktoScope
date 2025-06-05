2f48efbfbc96d9d25442744133b6c2997ae7f741
Sonny Piers <sonny@fairscope.com>
2025-06-05 10:14:05 +0000

    node-red: Add WIP list-segmentations node

--- node-red/nodes/api/db.js
+++ node-red/nodes/api/db.js
@@ -1,7 +1,8 @@
 import { join } from "path"
-import { opendir, readFile } from "fs/promises"
+import { opendir, readFile, access, constants } from "fs/promises"
 
-const PATH_ACQUISITION = "/home/pi/data/img"
+const PATH_ACQUISITION = "/home/pi/data/img/"
+const PATH_SEGMENTATION = "/home/pi/data/objects/"
 
 export async function listAcquisitions() {
   let acquisitions = []
@@ -88,7 +89,7 @@ async function getAcquisitionFromPath(path) {
     metadata.acq_id.split(sample_id + "_")[1] || metadata.acq_id
   const operator_name = metadata.sample_operator
   const image_acquired_count = metadata.acq_nb_frame
-  const is_segmented = null // TODO
+  const is_segmented = await isAcquisitionSegmented(path)
 
   const acquisition = {
     project_name,
@@ -100,6 +101,20 @@ async function getAcquisitionFromPath(path) {
   }
 
   return acquisition
+}
+
+async function isAcquisitionSegmented(path) {
+  // FIXME segmentation date may be different than acquisition
+  const segmentation_path = path.replace(PATH_ACQUISITION, PATH_SEGMENTATION)
+
+  try {
+    await access(segmentation_path, constants.F_OK)
+  } catch (err) {
+    if (err.code !== "ENOENT") throw err
+    return false
+  }
+
+  return true
 }
 
 // Looks like we can use acq_nb_frame instead
@@ -114,3 +129,75 @@ async function getAcquisitionFromPath(path) {
 //   }
 //   return c
 // }
+
+export async function listSegmentations() {
+  let segmentations = []
+
+  let fsdir
+  try {
+    fsdir = await opendir(PATH_SEGMENTATION)
+  } catch (err) {
+    if (err.code !== "ENOENT") throw err
+    return segmentations
+  }
+
+  for await (const d of fsdir) {
+    if (!d.isDirectory()) continue
+    segmentations.push(...(await listSegmentationsForDate(d.name)))
+  }
+
+  return segmentations
+}
+
+async function listSegmentationsForDate(date) {
+  let segmentations = []
+
+  let fsdir
+
+  try {
+    fsdir = await opendir(join(PATH_SEGMENTATION, date))
+  } catch (err) {
+    if (err.code !== "ENOENT") throw err
+    return segmentations
+  }
+
+  for await (const d of fsdir) {
+    if (!d.isDirectory()) continue
+    segmentations.push(
+      ...(await listSegmentationsFromDirectory(
+        join(PATH_SEGMENTATION, date, d.name)
+      ))
+    )
+  }
+
+  return segmentations
+}
+
+async function listSegmentationsFromDirectory(dir_path) {
+  let segmentations = []
+
+  let fsdir
+
+  try {
+    fsdir = await opendir(dir_path)
+  } catch (err) {
+    if (err.code !== "ENOENT") throw err
+    return segmentations
+  }
+
+  for await (const d of fsdir) {
+    if (!d.isDirectory()) continue
+
+    const path = join(dir_path, d.name)
+    const segmentation = {
+      project_name: path,
+      sample_id: path,
+      acquisition_id: path,
+    }
+    segmentations.push(segmentation)
+  }
+
+  return segmentations
+}
+
+listSegmentations().then(console.log).catch(console.error)
