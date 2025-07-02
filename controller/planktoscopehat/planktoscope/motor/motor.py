PlanktoScope/PlanktoScope

commit 8e3874bb5926f855318bcdbc202575d955396a7e

controller: Cleanup motor code (#607)
While working on stall detection, I learned a bunch about how TMC5160
drivers work.
I noticed unused code and unnecessary abstractions.

* Removes `stepper.py`
* Move used code from `stepper.py` into `motor.py`
* Remove unused `step` parameter

controller/planktoscopehat/planktoscope/motor/motor.py
@@ -1,77 +1,42 @@
 # mypy: ignore-errors
 
 import time
+from spidev import SpiDev
 
 from gpiozero import DigitalOutputDevice
 
 from . import registers as reg
 
+"""Step forward"""
+FORWARD = 1
+""""Step backward"""
+BACKWARD = 2
+
 
 class Motor:
-    def __init__(self, pin: int, spi):
-        # Setting the CS and enable pins according to the motor number called
+    def __init__(self, pin: int, spi_bus: int, spi_device: int):
         self.enable = DigitalOutputDevice(pin, active_high=False)
-        self.spi = spi
-        self.default_settings()
+
+        spi = SpiDev()
+        spi.open(spi_bus, spi_device)
+        # 1 MHZ
+        spi.max_speed_hz = 1000000
+        # 8 bits per word (32-bit word is broken into 4x 8-bit words)
+        spi.bits_per_word = 8
+        spi.loop = False
+        # SPI Mode 3
+        spi.mode = 3
+        self.__spi = spi
+
+        self.__goal = 0
+        self.__direction = ""
+        self.disable_motor()
 
     def enable_motor(self):
         self.enable.on()
 
     def disable_motor(self):
         self.enable.off()
-
-    def default_settings(self):
-        # Set default motor parameters
-
-        # MULTISTEP_FILT = 1, EN_PWM_MODE = 1 enables stealthChop
-        self.write(reg.GCONF, 0b0000000000001110)
-        # TOFF = 3, HSTRT = 4, HEND = 1, TBL = 2, CHM = 0 (spreadCycle)
-        self.write(reg.CHOPCONF, 0x000100C3)
-        # IHOLD = 1, IRUN = 5 (max current), IHOLDDELAY = 8
-        self.write(reg.IHOLD_IRUN, 0x00080501)
-        # TPOWERDOWN = 10: Delay before powerdown in standstill
-        self.write(reg.TPOWERDOWN, 0x0000000A)
-        # TPWMTHRS = 500
-        self.write(reg.TPWMTHRS, 0x000001F4)
-
-        self.reset_ramp_defaults()
-
-        # Position mode
-        self.write(reg.RAMPMODE, 0)
-        # Set current position to 0
-        self.write(reg.XACTUAL, 0)
-        # Set XTARGET to 0, which holds the motor at the current position
-        self.write(reg.XTARGET, 0)
-
-    # TODO: #105 add some more functionality...
-    # Add stallGuard + coolStep (datasheet page 52)
-
-    @property
-    def ramp_VSTART(self):
-        return self.__ramp_VSTART
-
-    @ramp_VSTART.setter
-    def ramp_VSTART(self, value: int):
-        self.write(reg.VSTART, value)
-        self.__ramp_VSTART = value
-
-    @property
-    def ramp_A1(self):
-        return self.__ramp_A1
-
-    @ramp_A1.setter
-    def ramp_A1(self, value: int):
-        self.write(reg.A1, value)
-        self.__ramp_A1 = value
-
-    @property
-    def ramp_V1(self):
-        return self.__ramp_V1
-
-    @ramp_V1.setter
-    def ramp_V1(self, value: int):
-        self.write(reg.V1, value)
-        self.__ramp_V1 = value
 
     @property
     def ramp_AMAX(self):
@@ -100,71 +65,6 @@ class Motor:
         self.write(reg.DMAX, value)
         self.__ramp_DMAX = value
 
-    @property
-    def ramp_D1(self):
-        return self.__ramp_D1
-
-    @ramp_D1.setter
-    def ramp_D1(self, value: int):
-        self.write(reg.D1, value)
-        self.__ramp_D1 = value
-
-    @property
-    def ramp_VSTOP(self):
-        return self.__ramp_VSTOP
-
-    @ramp_VSTOP.setter
-    def ramp_VSTOP(self, value: int):
-        self.write(reg.VSTOP, value)
-        self.__ramp_VSTOP = value
-
-    def write_ramp_params(self):
-        self.ramp_VSTART = self.ramp_VSTART
-        self.ramp_A1 = self.ramp_A1
-        self.ramp_V1 = self.ramp_V1
-        self.ramp_AMAX = self.ramp_AMAX
-        self.ramp_VMAX = self.ramp_VMAX
-        self.ramp_DMAX = self.ramp_DMAX
-        self.ramp_D1 = self.ramp_D1
-        self.ramp_VSTOP = self.ramp_VSTOP
-
-    def reset_ramp_defaults(self):
-        self.ramp_VSTART = 1
-        self.ramp_A1 = 2000
-        self.ramp_V1 = 3000
-        self.ramp_AMAX = 5000
-        self.ramp_VMAX = 100000
-        self.ramp_DMAX = 5000
-        self.ramp_D1 = 4000
-        self.ramp_VSTOP = 10
-
-    def enable_switch(self, direction: int):
-        # Configure limit switch.
-        # See datasheet for limit switch config default_settings
-
-        # Initialize list
-        setting_array = [0] * 12
-
-        # en_softstop = 1
-        setting_array[0] = 1
-
-        if direction == 1:
-            setting_array[6] = 1  # latch_l_active = 1
-            setting_array[11] = 1  # stop_l_enable = 1
-            error = False
-        elif direction == 2:
-            setting_array[4] = 1  # latch_r_active = 1
-            setting_array[10] = 1  # stop_r_enable = 1
-            error = False
-        else:
-            print("Not a valid input! Please use 1 (left), or 2 (right).")
-            error = True
-
-        if not error:
-            # Create binary string, to be correctly sent over SPI
-            switch_settings = int("".join(str(i) for i in setting_array), 2)
-            self.write(reg.SWMODE, switch_settings)
-
     def get_position(self) -> int:
         # Returns the current position of the motor relative to Home (0)
 
@@ -174,16 +74,6 @@ class Motor:
         current_position = self.twos_comp(current_position)
 
         return current_position
-
-    def get_latched_position(self) -> int:
-        # Returns the latched position when using limit switches.
-
-        latched_position = self.read(reg.XLATCH)
-
-        # Convert 2's complement to get signed number
-        latched_position = self.twos_comp(latched_position)
-
-        return latched_position
 
     def get_velocity(self) -> int:
         # Returns the current velocity of the motor (+ or -)
@@ -202,8 +92,6 @@ class Motor:
 
         self.position_mode()
 
-        self.write_ramp_params()
-
         # Position range is from -2^31 to +(2^31)-1
         maximum_position = (2**31) - 1
         minimum_position = -(2**31)
@@ -217,81 +105,6 @@ class Motor:
             print("Minimum position reached! Stopped at min value.")
 
         self.write(reg.XTARGET, position)
-
-    def calibrate_home(self, direction: int):
-        # Calibrate home by driving motor to limit switch.
-        # This is position-based rather than velocity based
-        # Direction: use 1 for left, 2 for right
-
-        self.enable_switch(direction)
-
-        # If the switch is active, move away from the switch until inactive
-        self.get_ramp_status()
-
-        switch_pressed = int(self.get_ramp_status.status_stop_l)
-
-        if direction == 1:
-            error = False
-
-            switch_pressed = int(self.get_ramp_status.status_stop_l)
-
-            if switch_pressed == 1:
-                # Move away from switch
-                self.go_to(512000)
-
-                while switch_pressed == 1:
-                    self.get_ramp_status()
-                    switch_pressed = int(self.get_ramp_status.status_stop_l)
-
-        elif direction == 2:
-            error = False
-
-            switch_pressed = int(self.get_ramp_status.status_stop_r)
-
-            if switch_pressed == 1:
-                # Move away from switch
-                self.go_to(-512000)
-
-                while switch_pressed == 1:
-                    self.get_ramp_status()
-                    switch_pressed = int(self.get_ramp_status.status_stop_r)
-
-        else:
-            print("Command not processed!")
-            error = True
-
-        if not error:
-            self.go_to(-2560000)
-
-            # Delay to let the motor ramp from standstill
-            time.sleep(0.1)
-
-            # Poll VACTUAL until it is 0
-            actual_velocity = self.get_velocity()
-
-            while actual_velocity != 0:
-                # time.sleep(0.001)
-                actual_velocity = self.get_velocity()
-
-            # Engage hold mode
-            self.hold_mode()
-
-            # Calcuate difference between latched position and actual position
-            actual_position = self.get_position()
-            latched_position = self.get_latched_position()
-
-            position_difference = actual_position - latched_position
-
-            # Write position_difference to XACTUAL to set home position
-            self.write(reg.XACTUAL, position_difference)
-
-            # Clear status_latch_l
-            self.write(reg.RAMPSTAT, 4)
-
-            # Go to 0 position: the exact position of switch activation
-            self.go_to(0)
-
-            print("Homing complete!")
 
     def move_velocity(self, dir: int, v_max: int = None, a_max: int = None):
         # Drive movor in velocity mode, positive or negative
@@ -332,30 +145,6 @@ class Motor:
     def position_mode(self):
         self.write(reg.RAMPMODE, 0)
 
-    def get_ramp_status(self):
-        self.read(reg.RAMPSTAT)
-
-        ramp_status = self.read(reg.RAMPSTAT)
-
-        ramp_status_binary = "{0:014b}".format(ramp_status)
-        ramp_status_array = list(ramp_status_binary)
-
-        # Parse response so individual registers can be referenced
-        Motor.get_ramp_status.status_sg = ramp_status_array[0]
-        Motor.get_ramp_status.second_move = ramp_status_array[1]
-        Motor.get_ramp_status.t_zerowait_active = ramp_status_array[2]
-        Motor.get_ramp_status.vzero = ramp_status_array[3]
-        Motor.get_ramp_status.position_reached = ramp_status_array[4]
-        Motor.get_ramp_status.velocity_reached = ramp_status_array[5]
-        Motor.get_ramp_status.event_pos_reached = ramp_status_array[6]
-        Motor.get_ramp_status.event_stop_sg = ramp_status_array[7]
-        Motor.get_ramp_status.event_stop_r = ramp_status_array[8]
-        Motor.get_ramp_status.event_stop_l = ramp_status_array[9]
-        Motor.get_ramp_status.status_latch_r = ramp_status_array[10]
-        Motor.get_ramp_status.status_latch_l = ramp_status_array[11]
-        Motor.get_ramp_status.status_stop_r = ramp_status_array[12]
-        Motor.get_ramp_status.status_stop_l = ramp_status_array[13]
-
     def read(self, address: int) -> int:
         # Read data from the SPI bus.
 
@@ -366,10 +155,10 @@ class Motor:
         # Clear write bit
         address_buffer[0] = address & 0x7F
 
-        self.spi.xfer2(address_buffer)
+        self.__spi.xfer2(address_buffer)
 
         # It will look like [address, 0, 0, 0, 0]
-        read_buffer = self.spi.xfer2(address_buffer)
+        read_buffer = self.__spi.xfer2(address_buffer)
 
         # Parse data returned from SPI transfer/read
         value = read_buffer[1]
@@ -396,7 +185,7 @@ class Motor:
         write_buffer[3] = 0xFF & (data >> 8)
         write_buffer[4] = 0xFF & data
 
-        return self.spi.xfer2(write_buffer)
+        return self.__spi.xfer2(write_buffer)
 
     def twos_comp(self, value: int, bits: int = 32) -> int:
         # if (value & (1 << (bits - 1))) != 0:
@@ -405,3 +194,85 @@ class Motor:
         #     signed_value = value
         # return signed_value
         return (value - (1 << bits)) if ((value & (1 << (bits - 1))) != 0) else value
+
+    @property
+    def speed(self):
+        return self.ramp_VMAX
+
+    @speed.setter
+    def speed(self, speed: int):
+        """Change the stepper speed
+
+        Args:
+            speed (int): speed of the movement by the stepper, in microsteps unit/s
+        """
+        self.ramp_VMAX = int(speed)
+
+    @property
+    def acceleration(self):
+        return self.ramp_AMAX
+
+    @acceleration.setter
+    def acceleration(self, acceleration: int):
+        """Change the stepper acceleration
+
+        Args:
+            acceleration (int): acceleration reachable by the stepper, in microsteps unit/s²
+        """
+        self.ramp_AMAX = int(acceleration)
+
+    @property
+    def deceleration(self):
+        return self.ramp_DMAX
+
+    @deceleration.setter
+    def deceleration(self, deceleration: int):
+        """Change the stepper deceleration
+
+        Args:
+            deceleration (int): deceleration reachable by the stepper, in microsteps unit/s²
+        """
+        self.ramp_DMAX = int(deceleration)
+
+    def at_goal(self):
+        """Is the motor at its goal
+
+        Returns:
+            Bool: True if position and goal are identical
+        """
+        return self.get_position() == self.__goal
+
+    def is_moving(self):
+        """is the stepper in movement?
+
+        Returns:
+          Bool: True if the stepper is moving
+        """
+        return self.get_velocity() != 0
+
+    def go(self, direction: int, distance: int):
+        """move in the given direction for the given distance
+
+        Args:
+          direction: gives the movement direction
+          distance:
+        """
+
+        self.__direction = direction
+        if self.__direction == FORWARD:
+            self.__goal = int(self.get_position() + distance)
+        elif self.__direction == BACKWARD:
+            self.__goal = int(self.get_position() - distance)
+        else:
+            raise ValueError(f'Unknown direction "{direction}".')
+        self.enable_motor()
+        self.go_to(self.__goal)
+
+    def shutdown(self):
+        """Shutdown everything ASAP"""
+        self.stop_motor()
+        self.disable_motor()
+        self.__goal = self.get_position()
+
+    def release(self):
+        self.disable_motor()

.github/workflows/build-controller.yml
@@ -9,11 +9,11 @@ on:
     tags:
       - 'v*'
     paths:
-      - 'controler/**'
+      - 'controller/**'
       - '.github/workflows/build-controller.yml'
   pull_request:
     paths:
-      - 'controler/**'
+      - 'controller/**'
       - '.github/workflows/build-controller.yml'
   merge_group:
   workflow_dispatch:

controller/planktoscopehat/planktoscope/focus.py
@@ -4,8 +4,9 @@ import time
 
 from loguru import logger
 
+from .motor.motor import Motor
+
 from . import mqtt
-from .stepper import stepper
 
 logger.info("planktoscope.focus is loaded")
 
@@ -32,7 +33,7 @@ class FocusProcess(multiprocessing.Process):
         self.focus_max_speed = configuration.get("focus_max_speed", self.focus_max_speed)
 
         # /dev/spidev0.1
-        self.focus_stepper = stepper(pin=5, spi_bus=0, spi_device=1, size=45)
+        self.focus_stepper = Motor(pin=5, spi_bus=0, spi_device=1)
 
         # Set stepper controller max speed
         self.focus_stepper.acceleration = 1000

controller/planktoscopehat/planktoscope/pump.py
@@ -4,8 +4,9 @@ import time
 
 from loguru import logger
 
+from .motor.motor import Motor
+
 from . import mqtt
-from .stepper import stepper
 
 logger.info("planktoscope.pump is loaded")
 
@@ -34,12 +35,12 @@ class PumpProcess(multiprocessing.Process):
         self.pump_max_speed = configuration.get("pump_max_speed", self.pump_max_speed)
 
         # /dev/spidev0.0
-        self.pump_stepper = stepper(pin=23, spi_bus=0, spi_device=0, size=0)
+        self.pump_stepper = Motor(pin=23, spi_bus=0, spi_device=0)
 
         # Set stepper controller max speed
         self.pump_stepper.acceleration = 2000
         self.pump_stepper.deceleration = self.pump_stepper.acceleration
-        self.pump_stepper.speed = self.pump_max_speed * self.pump_steps_per_ml * 256 / 60
+        self.pump_stepper.speed = int(self.pump_max_speed * self.pump_steps_per_ml * 256 / 60)
 
         logger.info("Pump initialisation is over")
 

controller/planktoscopehat/planktoscope/stepper.py
@@ -1,126 +1,0 @@
-# mypy: ignore-errors
-
-import spidev
-from loguru import logger
-
-from .motor.motor import Motor
-
-"""Step forward"""
-FORWARD = 1
-""""Step backward"""
-BACKWARD = 2
-
-
-class stepper:
-    def __init__(self, pin, spi_bus, spi_device, size=0):
-        """Initialize the stepper class
-
-        Args:
-            pin (int): pin of the motor
-            spi_bus (int)
-            spi_device (int)
-            size (int): maximum number of steps of this stepper (aka stage size). Can be 0 if not applicable
-        """
-        self.init_spi(spi_bus, spi_device)
-        self.__stepper = Motor(pin, self.__spi)
-        self.__size = size
-        self.__goal = 0
-        self.__direction = ""
-        self.__stepper.disable_motor()
-
-    def init_spi(self, bus, device):
-        spi = spidev.SpiDev()
-        # Open(Bus, Device)
-        spi.open(bus, device)
-        # 1 MHZ
-        spi.max_speed_hz = 1000000
-        # 8 bits per word (32-bit word is broken into 4x 8-bit words)
-        spi.bits_per_word = 8
-        spi.loop = False
-        # SPI Mode 3
-        spi.mode = 3
-        self.__spi = spi
-
-    def at_goal(self):
-        """Is the motor at its goal
-
-        Returns:
-            Bool: True if position and goal are identical
-        """
-        return self.__stepper.get_position() == self.__goal
-
-    def is_moving(self):
-        """is the stepper in movement?
-
-        Returns:
-          Bool: True if the stepper is moving
-        """
-        return self.__stepper.get_velocity() != 0
-
-    def go(self, direction: int, distance: int):
-        """move in the given direction for the given distance
-
-        Args:
-          direction: gives the movement direction
-          distance:
-        """
-        self.__direction = direction
-        if self.__direction == FORWARD:
-            self.__goal = int(self.__stepper.get_position() + distance)
-        elif self.__direction == BACKWARD:
-            self.__goal = int(self.__stepper.get_position() - distance)
-        else:
-            logger.error(f"The given direction is wrong {direction}")
-        self.__stepper.enable_motor()
-        self.__stepper.go_to(self.__goal)
-
-    def shutdown(self):
-        """Shutdown everything ASAP"""
-        self.__stepper.stop_motor()
-        self.__stepper.disable_motor()
-        self.__goal = self.__stepper.get_position()
-
-    def release(self):
-        self.__stepper.disable_motor()
-
-    @property
-    def speed(self):
-        return self.__stepper.ramp_VMAX
-
-    @speed.setter
-    def speed(self, speed):
-        """Change the stepper speed
-
-        Args:
-            speed (int): speed of the movement by the stepper, in microsteps unit/s
-        """
-        logger.debug(f"Setting stepper speed to {speed}")
-        self.__stepper.ramp_VMAX = int(speed)
-
-    @property
-    def acceleration(self):
-        return self.__stepper.ramp_AMAX
-
-    @acceleration.setter
-    def acceleration(self, acceleration):
-        """Change the stepper acceleration
-
-        Args:
-            acceleration (int): acceleration reachable by the stepper, in microsteps unit/s²
-        """
-        logger.debug(f"Setting stepper acceleration to {acceleration}")
-        self.__stepper.ramp_AMAX = int(acceleration)
-
-    @property
-    def deceleration(self):
-        return self.__stepper.ramp_DMAX
-
-    @deceleration.setter
-    def deceleration(self, deceleration):
-        """Change the stepper deceleration
-
-        Args:
-            deceleration (int): deceleration reachable by the stepper, in microsteps unit/s²
-        """
-        logger.debug(f"Setting stepper deceleration to {deceleration}")
-        self.__stepper.ramp_DMAX = int(deceleration)

commit 3d4e1ec035851252354fa052cf352605a4e25ebd

controller: Fix stepper regression (#687)
Regression from
https://github.com/PlanktoScope/PlanktoScope/commit/8e3874bb5926f855318bcdbc202575d955396a7e

controller/planktoscopehat/planktoscope/motor/motor.py
@@ -28,6 +28,8 @@ class Motor:
         spi.mode = 3
         self.__spi = spi
 
+        self.default_settings()
+
         self.__goal = 0
         self.__direction = ""
         self.disable_motor()
@@ -37,6 +39,34 @@ class Motor:
 
     def disable_motor(self):
         self.enable.off()
+
+    def default_settings(self):
+        # Set default motor parameters
+
+        # MULTISTEP_FILT = 1, EN_PWM_MODE = 1 enables stealthChopAdd commentMore actions
+        self.write(reg.GCONF, 0b0000000000001110)
+        # TOFF = 3, HSTRT = 4, HEND = 1, TBL = 2, CHM = 0 (spreadCycle)
+        self.write(reg.CHOPCONF, 0x000100C3)
+        # IHOLD = 1, IRUN = 5 (max current), IHOLDDELAY = 8
+        self.write(reg.IHOLD_IRUN, 0x00080501)
+        # TPOWERDOWN = 10: Delay before powerdown in standstill
+        self.write(reg.TPOWERDOWN, 0x0000000A)
+        # TPWMTHRS = 500
+        self.write(reg.TPWMTHRS, 0x000001F4)
+
+        self.reset_ramp_defaults()
+
+        # Position mode
+        self.write(reg.RAMPMODE, 0)
+        # Set current position to 0
+        self.write(reg.XACTUAL, 0)
+        # Set XTARGET to 0, which holds the motor at the current position
+        self.write(reg.XTARGET, 0)
+
+    def reset_ramp_defaults(self):
+        self.__ramp_AMAX = 5000
+        self.__ramp_VMAX = 100000
+        self.__ramp_DMAX = 5000
 
     @property
     def ramp_AMAX(self):