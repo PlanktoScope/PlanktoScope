93123f596bf896ad83a0d7c811c4919f9a0ccdb2
Sonny Piers <sonny@fairscope.com>
2025-07-09 07:30:36 +0000

    Fix build-os github action

--- os/setup.sh
+++ os/setup.sh
@@ -42,14 +42,6 @@ else
   panic "$description"
 fi
 
-description="configure networking"
-report_starting "$description"
-if "$build_scripts_root"/networking/install.sh; then
-  report_finished "$description"
-else
-  panic "$description"
-fi
-
 description="configure Raspberry Pi-specific hardware"
 report_starting "$description"
 if "$build_scripts_root"/platform-hardware/config.sh; then
