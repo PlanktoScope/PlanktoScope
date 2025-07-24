588a4ec5ef84cf1c6addbb7129a73f4f11cae3fa
Sonny Piers <sonny@fairscope.com>
2025-07-24 15:12:23 +0000

    f

--- os/setup.sh
+++ os/setup.sh
@@ -1,18 +1,14 @@
 #!/bin/bash -eux
 
-build_scripts_root=$(dirname "$(realpath "$BASH_SOURCE")")
-
 export PATH="$HOME/.local/bin:$PATH"
 export LANG="en_US.UTF-8"
 
 # The PlanktoScope monorepo is used for running and iterating on software components
 # https://github.com/PlanktoScope/planktoscope
-sudo cp -r "$repo_root" "$HOME/PlanktoScope"
-sudo chown -R pi:pi "$HOME/PlanktoScope"
+sudo chown -R "$USER:$USER" ..
+sudo cp -r .. "$HOME/PlanktoScope"
 
-sudo chown -R "$USER" "$build_scripts_root"/..
-
-just --justfile "$build_scripts_root"/../justfile base
-just --justfile "$build_scripts_root"/../justfile setup
+just --justfile ../justfile base
+just --justfile ../justfile setup
 
 just cleanup
