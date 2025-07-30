12253ce82a38bb46a91d2d4b7d482e1fbe3419ac
Sonny Piers <sonny@fairscope.com>
2025-07-30 14:36:22 +0000

    enable wifi on first boot

--- os/raspberry/firstboot.sh
+++ os/raspberry/firstboot.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
+rfkill unblock wifi || true
+nmcli radio wifi on || true
+
 rm -f /boot/firmware/pieeprom.sig
 rm -f /boot/firmware/pieeprom.upd
 rm -f /boot/firmware/RECOVERY.000
