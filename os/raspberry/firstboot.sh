512859fe4542ce8e828d440b547d0adf796868ee
Sonny Piers <sonny@fairscope.com>
2025-07-30 17:58:58 +0200

    enable wifi with raspi-config for now

--- os/raspberry/firstboot.sh
+++ os/raspberry/firstboot.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -eux
 
-rfkill unblock wifi || true
-nmcli radio wifi on || true
-nmcli connection up wlan0-hotspot
-
 rm -f /boot/firmware/pieeprom.sig
 rm -f /boot/firmware/pieeprom.upd
 rm -f /boot/firmware/RECOVERY.000
