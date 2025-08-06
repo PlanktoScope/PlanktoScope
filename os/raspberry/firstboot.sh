3b71b1a68f32a2818ec0851a4b90f33e1236eb74
Sonny Piers <sonny@fairscope.com>
2025-08-06 19:34:14 +0200

    f

--- os/raspberry/firstboot.sh
+++ os/raspberry/firstboot.sh
@@ -4,3 +4,4 @@ rm -f /boot/firmware/pieeprom.sig
 rm -f /boot/firmware/pieeprom.upd
 rm -f /boot/firmware/RECOVERY.000
 rm -f /boot/firmware/recovery.bin
+nmcli radio wifi on || true # See also network/justfile
