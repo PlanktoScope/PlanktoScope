cfdb51d754234f18a8965d93b54dec3e0fde9cc2
Sonny Piers <sonny@fairscope.com>
2025-09-04 11:55:13 +0200

    f

--- os/developer-mode/install-actionlint.sh
+++ os/developer-mode/install-actionlint.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/rhysd/actionlint/releases/download/v1.7.7/actionlint_1.7.7_linux_arm64.tar.gz -P /tmp
 cd /tmp && tar -xzf /tmp/actionlint_1.7.7_linux_arm64.tar.gz actionlint
-sudo cp actionlint /usr/local/bin/actionlint
+sudo cp /tmp/actionlint /usr/local/bin/actionlint
--- os/developer-mode/install-config-file-validator.sh
+++ os/developer-mode/install-config-file-validator.sh
@@ -4,4 +4,4 @@
 
 wget https://github.com/Boeing/config-file-validator/releases/download/v1.8.1/validator-v1.8.1-linux-arm64.tar.gz -P /tmp
 cd /tmp && tar -xzf /tmp/validator-v1.8.1-linux-arm64.tar.gz validator
-sudo cp validator /usr/local/bin/validator
+sudo cp /tmp/validator /usr/local/bin/validator
