b6e506c0a8e7ea9b96d61c341c884ad049a7a1b8
Sonny Piers <sonny@fairscope.com>
2025-09-17 12:59:18 +0000

    f

--- os/developer-mode/configure.mjs
+++ os/developer-mode/configure.mjs
@@ -6,10 +6,6 @@ import chalk from "chalk"
 import { $ } from "execa"
 import input from "@inquirer/input"
 
-function $verbose(cmd) {
-  console.log(cmd)
-}
-
 console.log(
   "Setting up Git Identity",
   chalk.dim(
@@ -24,7 +20,7 @@ if (!user_name) {
   user_name = await input({
     message: `Please enter your name. ${chalk.dim(`e.g. John Doe`)}\n> `,
   })
-  await $({ verbose: "short" })`git config --global user.name ${user_name}`
+  await $`git config --global user.name ${user_name}`
 }
 
 let { stdout: user_email } = await $({
@@ -34,12 +30,12 @@ if (!user_email) {
   user_email = await input({
     message: `Your email address? eg ${chalk.dim(`e.g. john.doe@example.edu`)}\n> `,
   })
-  await $({ verbose: "short" })`git config --global user.email ${user_email}`
+  await $`git config --global user.email ${user_email}`
 }
 
 let { stdout: push_autoSetupRemote } = await $({
   reject: false,
 })`git config --global push.autoSetupRemote`
 if (!push_autoSetupRemote) {
-  await $({ verbose: "short" })`git config --global push.autoSetupRemote true`
+  await $`git config --global push.autoSetupRemote true`
 }
