9fd6c3d28dd1fa2a537fd6eb8286328f8cf2bad8
Sonny Piers <sonny@fairscope.com>
2025-09-24 16:46:50 +0200

    Improve bookmarks

--- frontend/src/pages/bookmarks.jsx
+++ frontend/src/pages/bookmarks.jsx
@@ -19,7 +19,74 @@ export default function Bookmarks() {
       </header>
 
       <main>
+        <h2>Users</h2>
+
         <ul>
+          <li>
+            <a href={makeLocalUrl("/")} target="_blank">
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a href={makeUrl("/ps/data/browse/files/")} target="_blank">
+              Gallery
+              {/* FIXME */}
+            </a>
+          </li>
+          <li>
+            <a href={makeUrl("/ps/data/browse/")} target="_blank">
+              File Browser
+              {/* FIXME */}
+            </a>
+          </li>
+          <li>
+            <a href={makeUrl("/ps/docs/")} target="_blank">
+              Docs
+            </a>
+          </li>
+        </ul>
+
+        <h2>Developers</h2>
+
+        <ul>
+          <li>
+            <a
+              href={makeUrl("/admin/cockpit/system/logs#/?priority=info")}
+              target="_blank"
+            >
+              Logs
+            </a>
+          </li>
+          <li>
+            <a href={makeUrl("/admin/cockpit/system/terminal")} target="_blank">
+              Terminal
+            </a>
+          </li>
+
+          <li>
+            <a href={makeUrl("/admin/cockpit/")} target="_blank">
+              Cockpit
+            </a>
+          </li>
+          <li>
+            <a href={makeUrl("/admin/fs/")} target="_blank">
+              File Browser
+            </a>
+          </li>
+          <li>
+            <a href={makeUrl("/admin/ps/node-red-v2/")} target="_blank">
+              Node-RED flow editor
+            </a>
+          </li>
+          <li>
+            <a
+              href={makeUrl("/ps/processing/segmenter/streams/object.mjpg")}
+              target="_blank"
+            >
+              {/* FIXME does it work? */}
+              Last segmented object
+            </a>
+          </li>
           <li>
             <a href={makeLocalUrl("/bootstrap")} target="_blank">
               Bootstrap
@@ -28,11 +95,6 @@ export default function Bookmarks() {
           <li>
             <a href={makeLocalUrl("/setup")} target="_blank">
               Setup
-            </a>
-          </li>
-          <li>
-            <a href={makeLocalUrl("/gallery")} target="_blank">
-              Gallery
             </a>
           </li>
           <li>
@@ -45,53 +107,24 @@ export default function Bookmarks() {
               Node-RED Dashboard
             </a>
           </li>
+        </ul>
+
+        <h2>Experiments</h2>
+
+        <ul>
           <li>
-            <a href={makeUrl("/ps/data/browse/")} target="_blank">
-              File Manager
-            </a>
-          </li>
-          <li>
-            <a
-              href={makeUrl("/admin/ps/device-backend-logs/browse/")}
-              target="_blank"
-            >
-              Backend logs
-            </a>
-          </li>
-          <li>
-            <a href={makeUrl("/ps/docs/")} target="_blank">
-              Docs
-            </a>
-          </li>
-          <li>
-            <a href={makeUrl("/admin/cockpit/")} target="_blank">
-              Cockpit
-            </a>
-          </li>
-          <li>
-            <a href={makeUrl("/admin/fs/")} target="_blank">
-              File Management
-            </a>
-          </li>
-          <li>
-            <a href={makeUrl("/admin/ps/node-red-v2/")} target="_blank">
-              Node-RED flow editor
-            </a>
-          </li>
-          <li>
-            <a
-              href={makeUrl("/ps/hal/camera/streams/preview.mjpg")}
-              target="_blank"
-            >
+            <a href={makeLocalUrl("/preview")} target="_blank">
               Camera preview
             </a>
           </li>
           <li>
-            <a
-              href={makeUrl("/ps/processing/segmenter/streams/object.mjpg")}
-              target="_blank"
-            >
-              Last segmented object
+            <a href={makeLocalUrl("/gallery")} target="_blank">
+              Ecotaxa Project Gallery
+            </a>
+          </li>
+          <li>
+            <a href={makeLocalUrl("/developer-mode")} target="_blank">
+              Developer Mode
             </a>
           </li>
         </ul>
