ace9ffa13e135eae8b7928796a2487f6112ac992
Sonny Piers <sonny@fairscope.com>
2025-10-07 12:20:36 +0000

    Move hw details

--- controller/light/main.py
+++ controller/light/main.py
@@ -5,39 +5,20 @@ import aiomqtt
 import sys
 import signal
 
-import board  # type: ignore
-import busio  # type: ignore
-from adafruit_mcp4725 import MCP4725  # type: ignore
-
 import helpers
 
-MCP4725_ADDR = 0x60
-# Proportional 0 to 5V
-VALUE_MIN = 0
-VALUE_MAX = 65535
-VOLTAGE_MIN = 0
-# if you run it from 3.3V, the output range is 0-3.3V. If you run it from 5V the output range is 0-5V.
-VOLTAGE_MAX = 5
+from . import MCP4725 as led
+
 
 client = None
 loop = asyncio.new_event_loop()
-i2c = busio.I2C(board.SCL, board.SDA)
-dac = MCP4725(i2c, address=MCP4725_ADDR)
-
-
-def map_to_voltage(value):
-    return (value / VALUE_MAX) * VOLTAGE_MAX
-
-
-def map_to_adc(voltage):
-    return int((voltage / VOLTAGE_MAX) * VALUE_MAX)
 
 
 async def start() -> None:
     if (await helpers.get_hat_version()) != 3.3:
         sys.exit()
 
-    dac.value = 0
+    led.init()
     global client
     client = aiomqtt.Client(hostname="localhost", port=1883, protocol=aiomqtt.ProtocolVersion.V5)
     async with client:
@@ -67,27 +48,27 @@ async def handle_action(action: str) -> None:
     elif action == "off":
         await off()
     elif action == "save":
-        dac.save_to_eeprom()
-
+        if hasattr(led, 'save')
+            led.save()
 
 async def on() -> None:
-    dac.value = VALUE_MAX
+    led.on()
     await publish_status()
 
 
 async def off() -> None:
-    dac.value = VALUE_MIN
+    led.off()
     await publish_status()
 
 
 async def publish_status() -> None:
-    payload = {"status": "Off" if dac.value == 0 else "On"}
+    payload = {"status": "Off" if led.is_on() else "On"}
     await client.publish(topic="status/light", payload=json.dumps(payload), retain=True)
 
 
 async def stop() -> None:
     await off()
-    i2c.deinit()
+    led.deinit()
     loop.stop()
 
 
