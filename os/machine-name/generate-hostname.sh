f69ceb43fdd46186e2373c29564a9ac28631490c
Sonny Piers <sonny@fairscope.com>
2025-11-19 17:44:45 +0100

    f

--- os/machine-name/generate-hostname.sh
+++ os/machine-name/generate-hostname.sh
@@ -10,4 +10,4 @@ printf "%s" "$hostname" > /etc/hostname
 
 sudo sed -i "s/raspberrypi/$hostname/g" /etc/hosts
 sudo sed -i "s/raspberrypi/$hostname/g" /etc/cockpit/cockpit.conf
-sudo sed -i "s/raspberrypi/$hostname/g" /etc/NetworkManager/system-connections/wlan0-hotspot.nmconnection
+sudo sed -i "s/raspberrypi/PlanktoScope $machine_name/g" /etc/NetworkManager/system-connections/wlan0-hotspot.nmconnection
