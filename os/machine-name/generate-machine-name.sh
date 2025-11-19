7c2a2900d96f641563d175f2af8c7ffb319fb5f3
Sonny Piers <sonny@fairscope.com>
2025-11-19 14:09:13 +0100

    os: Simplify hostname

--- os/machine-name/generate-machine-name.sh
+++ os/machine-name/generate-machine-name.sh
@@ -1,44 +1,10 @@
 #!/bin/bash -eu
 # shellcheck disable=all
 
-machine_name=""
-
-# Allow preempting automatic machine-name generation behavior with /etc/machine-name:
-if [ -f /etc/machine-name ]; then
-  machine_name="$(sed 's~#.*$~~g' /etc/machine-name | tr -d '[:space:]')"
-fi
-
-if [ ! -z "$machine_name" ]; then
-  echo "A machine name was manually specified in /etc/machine-name: $machine_name"
-elif [ -f /sys/firmware/devicetree/base/serial-number ] && [ -x /usr/local/bin/machine-name ]; then
-  # Automatically generate a machine-name from the RPi serial number if needed:
-  serial_number="$(tr -d '\0' </sys/firmware/devicetree/base/serial-number | cut -c 9-)"
-  machine_name="$(
-    /usr/local/bin/machine-name name --format=hex --sn="$serial_number" ||
-      # fall back to English in case the language setting is unrecognized:
-      LANG=en_US.UTF-8 /usr/local/bin/machine-name name --format=hex --sn="$serial_number"
-  )"
-fi
-
-if [ "$machine_name" = "" ] &&
-  [ -f /etc/machine-id ] &&
-  [ -x /usr/bin/sha256sum ] &&
-  [ -x /usr/local/bin/machine-name ]; then
-  serial_number="$(sha256sum /etc/machine-id | cut -c 1-8)"
-  machine_name="$(
-    /usr/local/bin/machine-name name --format=hex --sn="$serial_number" ||
-      # fall back to English in case the language setting is unrecognized:
-      LANG=en_US.UTF-8 /usr/local/bin/machine-name name --format=hex --sn="$serial_number"
-  )"
-fi
-
-if [ "$machine_name" = "" ]; then
-  echo "Warning: a machine name could not be generated!"
-  if [ -f /run/machine-name ]; then
-    echo "Using default machine name: $(cat /run/machine-name)"
-  fi
-  exit 1
-fi
+# Automatically generate a machine-name from the RPi serial number
+serial_number="$(tr -d '\0' </sys/firmware/devicetree/base/serial-number | cut -c 9-)"
+machine_name="$(LANG=en_US.UTF-8 /usr/local/bin/machine-name name --format=hex --sn="$serial_number")"
+machine_name="$(echo $machine_name | sed 's/-[0-9]*$//')"
 
 echo "Machine name: $machine_name"
 printf "%s" "$machine_name" >/run/machine-name
