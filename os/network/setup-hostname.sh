e3562537d73576b9859aeb1c2c5aff35cbb0b22d
Sonny Piers <sonny@fastmail.net>
2025-11-26 15:39:49 +0000

    machine name

--- os/network/setup-hostname.sh
+++ os/network/setup-hostname.sh
@@ -3,6 +3,6 @@
 hostname=$(cat /etc/hostname)
 machine_name=$(cat /run/machine-name)
 
-sudo sed -i "s/raspberrypi/$hostname/g" /etc/hosts
-sudo sed -i "s/raspberrypi/$hostname/g" /etc/cockpit/cockpit.conf
-sudo sed -i "s/raspberrypi/PlanktoScope $machine_name/g" /etc/NetworkManager/system-connections/wlan0-hotspot.nmconnection
+sed -i "s/raspberrypi/$hostname/g" /etc/hosts || true
+sed -i "s/raspberrypi/$hostname/g" /etc/cockpit/cockpit.conf || true
+sed -i "s/raspberrypi/PlanktoScope $machine_name/g" /etc/NetworkManager/system-connections/wlan0-hotspot.nmconnection || true
