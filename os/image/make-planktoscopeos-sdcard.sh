ad86738422780903c2a6b14e096bc08f41e8a0e6
Sonny Piers <sonny@fairscope.com>
2025-11-27 18:20:14 +0100

    f

--- os/image/make-planktoscopeos-sdcard.sh
+++ os/image/make-planktoscopeos-sdcard.sh
@@ -4,4 +4,9 @@
 dd bs=1M if=/dev/mmcblk0 status=progress conv=fdatasync of=pkos.img
 
 # shrink
-pishrink.sh pkos.img
+pishrink.sh -Za pkos.img
+
+# compress
+xz -T0 -9 -k pkos.img
+
+# upload pkos.img.xz
