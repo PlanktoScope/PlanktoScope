81eefcbe2432fc6e39420a419f7e23fae42d2542
Sonny Piers <sonny@fairscope.com>
2025-12-10 11:22:43 +0000

    f

--- backend/src/network/network.js
+++ backend/src/network/network.js
@@ -1,6 +1,6 @@
-import { publish, procedure } from "../../lib/mqtt.js"
+import { publish, procedure } from "../../../lib/mqtt.js"
 
-import { NetworkManager } from "../../lib/network.js"
+import { NetworkManager } from "../../../lib/network.js"
 
 const networkmanager = new NetworkManager()
 await networkmanager.init()
@@ -8,7 +8,7 @@ await networkmanager.init()
 async function publishAccessPoints() {
   try {
     const wifis = await networkmanager.getWifis()
-    publish("config/wifis", wifis, null, { retain: true })
+    publish("network/wifi/list", wifis, null, { retain: true })
   } catch (err) {
     console.error(err)
   }
@@ -22,13 +22,15 @@ networkmanager.DeviceWireless.on("AccessPointRemoved", (/*access_point*/) => {
   publishAccessPoints()
 })
 
-await procedure("config/wifis/scan", async () => {
+await procedure("network/wifi/scan", async () => {
   await networkmanager.scan()
 })
 
-await procedure("config/wifis/connect", async (data) => {
+await procedure("network/wifi/connect", async (data) => {
   await networkmanager.connectToWifi(data.path)
 })
+
+//
 ;(async () => {
   await publishAccessPoints()
   await networkmanager.scan()
