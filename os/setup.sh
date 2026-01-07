66e25d62b9b40ab78e2a68c07d2c01c0c6b6a24d
Sonny Piers <sonny@fairscope.com>
2026-01-07 17:54:27 +0100

    change date

--- os/setup.sh
+++ os/setup.sh
@@ -4,7 +4,7 @@
 # It is meant to be run on a specific Raspberry OS Pi OS standard installation.
 
 line=$(head -n 1 /etc/rpi-issue)
-date="2025-11-24"
+date="2025-12-04"
 expected="Raspberry Pi reference $date"
 
 if [ "$line" != "$expected" ]; then
