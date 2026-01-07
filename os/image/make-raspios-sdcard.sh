ffaf8ae2578a2a6ee86a558c9552933b2966b743
Sonny Piers <sonny@fairscope.com>
2026-01-07 17:06:59 +0100

    update

--- os/image/make-raspios-sdcard.sh
+++ os/image/make-raspios-sdcard.sh
@@ -1,30 +1,34 @@
 #!/bin/bash -eux
 
+file=2025-11-24-raspios-trixie-arm64-lite.img.xz
+url=https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2025-11-24/$file
+device=/dev/mmcblk0
+sha256="1d448a6e665e1ae8100bc28b35408619ec626a2fddfd6579ec99e7996fa09a56"
+
 # download raspios
-wget -c https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2025-11-24/2025-11-24-raspios-trixie-arm64-lite.img.xz
+wget -c -nc "${url}"
 
 # download signature
-wget -c https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2025-11-24/2025-11-24-raspios-trixie-arm64-lite.img.xz.sha256
+wget -c -nc "${url}.sha256"
 
 # make sure signature hasn't changed
-head -c 65 2025-11-24-raspios-trixie-arm64-lite.img.xz.sha256 | grep -qx "1d448a6e665e1ae8100bc28b35408619ec626a2fddfd6579ec99e7996fa09a56 "
+head -c 64 ${file}.sha256 | grep -qx "${sha256}"
 
 # verify signature
-cat 2025-11-24-raspios-trixie-arm64-lite.img.xz.sha256 | sha256sum --check
+cat ${file}.sha256 | sha256sum --check
 
 # unmount
-umount -q /dev/mmcblk0 || true
-umount -q /dev/mmcblk0p1 || true
-umount -q /dev/mmcblk0p2 || true
+umount -q ${device}? || true
+umount -q ${device} || true
 
 # new empty dos partition table
-echo 'label: dos' | sfdisk /dev/mmcblk0
+echo 'label: dos' | sfdisk $device
 
 # write raspios
-xzcat 2025-11-24-raspios-trixie-arm64-lite.img.xz | dd bs=1M of=/dev/mmcblk0 status=progress conv=fdatasync
+xzcat $file | dd bs=1M of=$device status=progress conv=fdatasync
 
 # mount boot partition
-mount /dev/mmcblk0p1 /mnt
+mount ${device}p1 /mnt
 
 # create user
 echo "pi:$(echo 'copepode' | openssl passwd -6 -stdin)" > /mnt/userconf
