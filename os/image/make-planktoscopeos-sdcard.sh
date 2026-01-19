9276efcf0a7fc313928ae93aece19e50611f7ddf
Sonny Piers <sonny@fairscope.com>
2026-01-19 13:49:19 +0000

    os: Prototype with exfat for data

--- os/image/make-planktoscopeos-sdcard.sh
+++ os/image/make-planktoscopeos-sdcard.sh
@@ -7,6 +7,16 @@ file=$name.img
 # save raspios sdcard
 dd bs=1M if="$device" status=progress conv=fdatasync of="$file"
 
+# resize
+# TODO resize /dev/mmcblk0p1
+# TODO make /dev/mmcblk0p1 exfat label PKSCOPE
+
+# TODO
+
+# Add one of to /etc/fstab
+# LABEL=PKSCOPE  /home/pi/data      exfat   defaults,noatime,nofail,umask=000  0  2
+# /dev/mmcblk0p3  /home/pi/data      exfat   defaults,noatime,nofail,umask=000  0  2
+
 # shrink
 pishrink.sh -Za "$file"
 
